0aa8bf447cece0543d0160bcc685bd67944cccd3
FIL708 <filip.zebrowski4013@gmail.com>
2023-03-26 15:08:58 +0200

    setup client

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const App = () => {
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function main() {
+  const root = document.createElement('div');
+  document.querySelector('body').appendChild(root);
+  ReactDOM.createRoot(root).render(<App />);
+}
+
+main();
