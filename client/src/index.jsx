5328b0af35bc7aa26620098e8fbc75c8c3e975f7
FIL708 <filip.zebrowski4013@gmail.com>
2023-03-28 18:50:51 +0200

    create routes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Home } from './pages/index.js';
 
-const App = () => {
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/adoption" element={<h1>Adoption</h1>} />
-        <Route path="/gallery" element={<h1>Gallery</h1>} />
-        <Route path="/contact" element={<h1>Contact</h1>} />
-        <Route path="/signup" element={<h1>SignUp</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>;
-};
+function App() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/adoption" element={<Home />} />
+          <Route path="/gallery" element={<h1>Gallery</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="/signup" element={<h1>SignUp</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
 
-function main() {
+async function main() {
   const root = document.createElement('div');
   document.querySelector('body').appendChild(root);
   ReactDOM.createRoot(root).render(<App />);
