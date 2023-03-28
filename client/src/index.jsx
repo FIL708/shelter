caf5d87bc1c447a9c96784ebc9166049a8c7685a
FIL708 <filip.zebrowski4013@gmail.com>
2023-03-28 18:58:04 +0200

    add pages to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { Home } from './pages/index.js';
+import {
+  Home,
+  Adoption,
+  Gallery,
+  Contact,
+  Signup,
+  Login,
+} from './pages/index.js';
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/adoption" element={<Home />} />
-          <Route path="/gallery" element={<h1>Gallery</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
-          <Route path="/signup" element={<h1>SignUp</h1>} />
-          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/adoption" element={<Adoption />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
