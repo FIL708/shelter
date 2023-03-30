f0a4edbebe30d35abe0865846a091d5480c1b3e4
FIL708 <filip.zebrowski4013@gmail.com>
2023-03-30 20:40:48 +0200

    create subtitle component

--- client/src/components/welcome-section/welcome-section.jsx
+++ client/src/components/welcome-section/welcome-section.jsx
@@ -2,10 +2,13 @@ import './welcome-section.css';
 
 import logo from './assets/logo.svg';
 
+import { Subtitle } from '../index.js';
+
 export default function WelcomeSection() {
   return (
     <section className="welcome-section">
       <img src={logo} alt="welcome section logo" />
+      <Subtitle title="Welcome!" />
     </section>
   );
 }
