77e1e655e5180f53db4df5d3edd9e91524f0e5ad
FIL708 <filip.zebrowski4013@gmail.com>
2023-03-31 11:05:04 +0200

    import icon to test page

--- client/src/components/icon/icon.jsx
+++ client/src/components/icon/icon.jsx
@@ -1,3 +1,7 @@
-export default function Icon() {
-  return <div>icon</div>;
+import chooseIcon from './choose-icon.js';
+
+export default function Icon({ type, height, width, fill }) {
+  const IconComponent = chooseIcon(type);
+  if (IconComponent === undefined) return null;
+  return <IconComponent height={height} width={width} fill={fill} />;
 }
