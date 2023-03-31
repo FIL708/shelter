6d841f5cb9e5b8b4f809bcd855ab2344d7e375c1
FIL708 <filip.zebrowski4013@gmail.com>
2023-03-31 13:07:16 +0200

    import button to test page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from './header/header.jsx';
 export { default as Page } from './page/page.jsx';
 export { default as Navbar } from './navbar/navbar.jsx';
 export { default as Icon } from './icon/icon.jsx';
+export { default as Button } from './button/button.jsx';
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,10 +1,14 @@
-import { Page, Icon } from '../components';
+import { Page, Icon, Button } from '../components';
 
 export default function Test() {
+  const handelClick = () => {
+    console.log('clicked!');
+  };
   return (
     <Page>
       <h1>Test page</h1>
       <Icon type="option" size="50px" fill="pink" />
+      <Button onClick={handelClick} text="Button" iconType="" />
     </Page>
   );
 }
