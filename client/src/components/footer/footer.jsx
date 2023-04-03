ea6c74593569d802930f6cc216b4d29a932ec351
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-03 17:32:00 +0200

    set footer layout

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -8,18 +8,20 @@ export default function Footer() {
     <footer className="footer">
       <img src={logo} alt="shelter logo" className="footer__logo" />
       <hr className="footer__divider" />
-      <FooterContactSection />
-      <section>
-        <h2 className="footer__section-title">Our mission:</h2>
-        <p>
-          Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-          vulputate libero et velit interdum, ac aliquet odio mattis. Class
-          aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Class aptent taciti sociosqu ad litora torquent
-          per conubia nostra, per inceptos himenaeos.
-        </p>
-      </section>
-      <FooterMediaSection />
+      <div className="footer__content">
+        <FooterContactSection />
+        <section>
+          <h2 className="footer__section-title">Our mission:</h2>
+          <p>
+            Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+            vulputate libero et velit interdum, ac aliquet odio mattis. Class
+            aptent taciti sociosqu ad litora torquent per conubia nostra, per
+            inceptos himenaeos. Class aptent taciti sociosqu ad litora torquent
+            per conubia nostra, per inceptos himenaeos.
+          </p>
+        </section>
+        <FooterMediaSection />
+      </div>
     </footer>
   );
 }
