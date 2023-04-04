5e228513f6f698f1fce3f18a4ee9b2a8567f972b
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-04 14:55:58 +0200

    add all comp for login form

--- client/src/components/login-form/login-form.jsx
+++ client/src/components/login-form/login-form.jsx
@@ -3,11 +3,18 @@ import { Textfield, Button } from '../index.js';
 
 export default function LoginForm() {
   return (
-    <form className="login-form">
-      <h2>Login</h2>
-      <Textfield name="email" label="Email" />
-      <Textfield name="password" label="Password" />
-      <Button text="Login" iconType="login" iconFill="" iconSize="15px" />
-    </form>
+    <>
+      <form className="login-form">
+        <Textfield name="email" label="Email" />
+        <Textfield name="password" type="password" label="Password" />
+        <Button
+          text="Login"
+          iconType="login"
+          iconFill="#fff2f2"
+          iconSize="18px"
+        />
+      </form>
+      <span>asdas</span>
+    </>
   );
 }
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,9 +1,10 @@
-import { Page } from '../components';
+import { Page, LoginForm } from '../components';
 
 export default function Login() {
   return (
     <Page>
       <h1>Login</h1>
+      <LoginForm />
     </Page>
   );
 }
