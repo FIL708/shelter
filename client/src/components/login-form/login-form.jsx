3874ded5b9bd5fb021b78e8920484447c6fc3c84
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-04 15:18:02 +0200

    add functionality

--- client/src/components/login-form/login-form.jsx
+++ client/src/components/login-form/login-form.jsx
@@ -1,17 +1,39 @@
 import './login-form.css';
 import { Textfield, Button } from '../index.js';
 
-export default function LoginForm() {
+export default function LoginForm({
+  loginHandler,
+  loginDataHandler,
+  formData,
+  validationHandler,
+  formIsValid,
+}) {
   return (
     <>
       <form className="login-form">
-        <Textfield name="email" label="Email" />
-        <Textfield name="password" type="password" label="Password" />
+        <Textfield
+          name="email"
+          label="Email"
+          onChange={(event) => loginDataHandler(event)}
+          value={formData.email}
+          onBlur={(event) => validationHandler(event, 'email')}
+          validation={formIsValid.email}
+        />
+        <Textfield
+          name="password"
+          type="password"
+          label="Password"
+          onChange={(event) => loginDataHandler(event)}
+          value={formData.password}
+          onBlur={(event) => validationHandler(event, 'password')}
+          validation={formIsValid.password}
+        />
         <Button
           text="Login"
           iconType="login"
           iconFill="#fff2f2"
           iconSize="18px"
+          onClick={loginHandler}
         />
       </form>
       <span>asdas</span>
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+import { inputValidator } from '../helpers';
 import { Page, LoginForm } from '../components';
 
 export default function Login() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [formIsValid, setFormIsValid] = useState({});
+  const loginHandler = () => {
+    console.log(formData);
+  };
+  const loginDataHandler = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  const validationHandler = (event, type) => {
+    const { name, value } = event.target;
+    const validationObject = inputValidator(value, type);
+    setFormIsValid((prev) => ({ ...prev, [name]: validationObject }));
+  };
   return (
     <Page>
       <h1>Login</h1>
-      <LoginForm />
+      <LoginForm
+        loginHandler={loginHandler}
+        loginDataHandler={loginDataHandler}
+        formData={formData}
+        formIsValid={formIsValid}
+        validationHandler={validationHandler}
+      />
     </Page>
   );
 }
