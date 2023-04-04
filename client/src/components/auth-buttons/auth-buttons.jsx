ece5ee36de5152fdcb0d899ab454db7db3c3c4ec
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-04 15:58:04 +0200

    styling auth buttons

--- client/src/components/auth-buttons/auth-buttons.jsx
+++ client/src/components/auth-buttons/auth-buttons.jsx
@@ -1,13 +1,32 @@
 import { Button } from '../index.js';
 import './auth-buttons.css';
 
-export default function AuthButtons({ text }) {
+export default function AuthButtons({
+  text,
+  googleHandler,
+  facebookHandler,
+  twitterHandler,
+}) {
   return (
     <div className="auth-buttons">
-      {text}
-      <Button iconType="" iconSize="30px" />
-      <Button iconType="" iconSize="30px" />
-      <Button iconType="" iconSize="30px" />
+      <h4 className="auth-buttons__text">{text}</h4>
+      <div className="auth-buttons__buttons">
+        <Button
+          iconType="round-google"
+          iconSize="35px"
+          onClick={googleHandler}
+        />
+        <Button
+          iconType="round-twitter"
+          iconSize="35px"
+          onClick={twitterHandler}
+        />
+        <Button
+          iconType="round-facebook"
+          iconSize="35px"
+          onClick={facebookHandler}
+        />
+      </div>
     </div>
   );
 }
