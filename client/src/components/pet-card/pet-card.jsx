fc33e807ed87157b178c5445525c6ab2d062d987
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-05 15:10:36 +0200

    add func to button

--- client/src/components/pet-card/pet-card.jsx
+++ client/src/components/pet-card/pet-card.jsx
@@ -1,38 +1,49 @@
+import { useState } from 'react';
 import { Button } from '../index.js';
 import PetCardPhoto from './pet-card-photo/pet-card-photo.jsx';
 import './pet-card.css';
 
-export default function PetCard({ url, isFavourite }) {
+export default function PetCard({
+  url,
+  isFavourite,
+  name,
+  location,
+  description,
+  createdAt,
+}) {
+  const [isFav, setIsFav] = useState(isFavourite);
+
   let iconType;
   let iconFill;
 
-  if (isFavourite) {
+  if (isFav) {
     iconType = 'full-heart';
     iconFill = '#9f3e3e';
   } else {
     iconType = 'heart';
     iconFill = '#7286d3';
   }
+  const toggleFav = () => {
+    setIsFav((prev) => !prev);
+  };
   return (
     <li className="pet-card">
       <PetCardPhoto url={url} />
       <div className="pet-card__content">
-        <h3 className="pet-card__name">Pet name</h3>
-        <h4 className="pet-car__location">Location</h4>
+        <h3 className="pet-card__name">{name}</h3>
+        <h4 className="pet-car__location">{location}</h4>
         <hr className="pet-card__divider" />
-        <p className="pet-car__disc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur a
-          ducimus iure accusamus recusandae opti...
-        </p>
+        <p className="pet-car__desc">{description}</p>
         <strong className="pet-card__note">
           looking for a home:{' '}
-          <span className="pet-card__date">20 march 2022</span>
+          <span className="pet-card__date">{createdAt}</span>
         </strong>
         <Button
           className="pet-card__button"
           iconType={iconType}
           iconSize="20px"
           iconFill={iconFill}
+          onClick={toggleFav}
         />
       </div>
     </li>
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,10 +1,19 @@
 import { Page, Subtitle, PetCard } from '../components';
 
 export default function Test() {
+  const pet = {
+    url: 'https://d.newsweek.com/en/full/1920025/cat-its-mouth-open.jpg?w=1600&h=1600&q=88&f=b7a44663e082b8041129616b6b73328d',
+    name: 'Reksio',
+    location: 'Pcim, Poland',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur a ducimus iure accusamus recusandae opti...',
+    isFavourite: true,
+    createdAt: '20 march 2022',
+  };
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <PetCard url="https://d.newsweek.com/en/full/1920025/cat-its-mouth-open.jpg?w=1600&h=1600&q=88&f=b7a44663e082b8041129616b6b73328d" />
+      <PetCard {...pet} />
     </Page>
   );
 }
