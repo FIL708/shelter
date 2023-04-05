be00362976c719aec4905999ee197b45f818d973
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-05 12:41:48 +0200

    add link to photo

--- client/src/components/pet-card/pet-card.jsx
+++ client/src/components/pet-card/pet-card.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import './pet-card.css';
 
 export default function PetCard({ url }) {
   return (
     <li className="pet-card">
-      <img src={url} alt="pet" className="pet-card-photo" />
+      <Link to="/">
+        <img src={url} alt="pet" className="pet-card-photo" />
+      </Link>
     </li>
   );
 }
