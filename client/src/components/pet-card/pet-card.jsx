d33a890ecd8a9004c3c2151c9e85b8e2e82688b1
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-05 14:21:08 +0200

    styling

--- client/src/components/pet-card/pet-card.jsx
+++ client/src/components/pet-card/pet-card.jsx
@@ -1,3 +1,4 @@
+import { Button } from '../index.js';
 import PetCardPhoto from './pet-card-photo/pet-card-photo.jsx';
 import './pet-card.css';
 
@@ -5,6 +6,25 @@ export default function PetCard({ url }) {
   return (
     <li className="pet-card">
       <PetCardPhoto url={url} />
+      <div className="pet-card__content">
+        <h3 className="pet-card__name">Pet name</h3>
+        <h4 className="pet-car__location">Location</h4>
+        <hr className="pet-card__divider" />
+        <p className="pet-car__disc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur a
+          ducimus iure accusamus recusandae opti...
+        </p>
+        <strong className="pet-card__note">
+          looking for a home:{' '}
+          <span className="pet-card__date">20 march 2022</span>
+        </strong>
+        <Button
+          className="pet-card__button"
+          iconType="heart"
+          iconSize="20px"
+          iconFill="#7286d3"
+        />
+      </div>
     </li>
   );
 }
