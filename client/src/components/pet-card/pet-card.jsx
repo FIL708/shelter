7eb5e42dd039cbbe2e9cacbb151f3a9ab969ded8
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-05 13:47:31 +0200

    photo styling + animation

--- client/src/components/pet-card/pet-card.jsx
+++ client/src/components/pet-card/pet-card.jsx
@@ -4,8 +4,9 @@ import './pet-card.css';
 export default function PetCard({ url }) {
   return (
     <li className="pet-card">
-      <Link to="/">
+      <Link to="/" className="pet-card__link">
         <img src={url} alt="pet" className="pet-card-photo" />
+        <p className="pet-card__hover-text">Click me!</p>
       </Link>
     </li>
   );
