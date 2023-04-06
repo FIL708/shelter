e028b3d3fc4d6e55d16c581f8183c1dcfddff916
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-06 15:52:28 +0200

    filtering abillity

--- client/src/pages/adoption.jsx
+++ client/src/pages/adoption.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
-import { Page, Subtitle, PetController } from '../components';
+import { useEffect, useState } from 'react';
+import { Page, Subtitle, PetController, PetCardList } from '../components';
+import pets from '../pets.json';
 
 export default function Adoption() {
+  const [petsData, setPetsData] = useState(pets);
   const [controlValues, setControlValues] = useState({
     mode: 'grid',
     species: 'all',
     numberOfPets: 9,
   });
+  useEffect(() => {
+    if (controlValues.species === 'all') {
+      setPetsData(pets);
+    } else if (controlValues.species === 'cats') {
+      setPetsData(pets.filter((pet) => pet.species === 'cat'));
+    } else {
+      setPetsData(pets.filter((pet) => pet.species === 'dog'));
+    }
+    console.log(controlValues);
+  }, [controlValues]);
   const handleControlValues = (event) => {
     const { name, value } = event.target;
     setControlValues((prev) => ({ ...prev, [name]: value }));
@@ -15,6 +27,7 @@ export default function Adoption() {
     <Page>
       <Subtitle text="Our Pets" main />
       <PetController values={controlValues} onChange={handleControlValues} />
+      <PetCardList pets={petsData} />
     </Page>
   );
 }
