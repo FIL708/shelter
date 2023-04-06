7e2d5fade853957faa619e711352955af9ee48a8
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-06 09:46:45 +0200

    button position

--- client/src/components/modal-wrapper/modal-wrapper.jsx
+++ client/src/components/modal-wrapper/modal-wrapper.jsx
@@ -4,7 +4,12 @@ import './modal-wrapper.css';
 export default function ModalWrapper({ children }) {
   return (
     <dialog className="modal-wrapper">
-      <Button className="modal-wrapper__button" />
+      <Button
+        className="modal-wrapper__button"
+        iconType="exit"
+        iconSize="30px"
+        iconFill="#fff2f2"
+      />
       {children}
     </dialog>
   );
