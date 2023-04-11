560e0b46c3edd2c5cad242b0afee71d9a5c79d12
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-11 16:30:25 +0200

    data chunking

--- client/src/pages/gallery.jsx
+++ client/src/pages/gallery.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import { GalleryCardList, Page, Subtitle } from '../components';
 import photos from '../photos.json';
+import getDataChunks from '../helpers/get-data-chunks';
 
 export default function Gallery() {
+  const [petPhotos, setPetPhotos] = useState(photos);
+  console.log(setPetPhotos);
+  const photosChunks = getDataChunks(petPhotos, 12);
   return (
     <Page>
       <Subtitle text="Gallery" main />
-      <GalleryCardList photos={photos} />
+      <GalleryCardList photos={photosChunks[0]} />
     </Page>
   );
 }
