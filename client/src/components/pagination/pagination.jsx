246410e7350bb5dc21c10fe83492fc02115d4a9e
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-11 11:57:37 +0200

    create pagination template

--- client/src/components/pagination/pagination.jsx
+++ client/src/components/pagination/pagination.jsx
@@ -1,5 +1,32 @@
+import { Button } from '../index.js';
 import './pagination.css';
 
 export default function Pagination() {
-  return <nav>pagination</nav>;
+  return (
+    <nav className="pagination">
+      <ul className="pagination__buttons-list">
+        <li>
+          <Button text="<" />
+        </li>
+        <li>
+          <Button text="1" />
+        </li>
+        <li>
+          <Button text="2" />
+        </li>
+        <li>
+          <Button text="3" />
+        </li>
+        <li>
+          <Button text="4" />
+        </li>
+        <li>
+          <Button text="5" />
+        </li>
+        <li>
+          <Button text=">" />
+        </li>
+      </ul>
+    </nav>
+  );
 }
