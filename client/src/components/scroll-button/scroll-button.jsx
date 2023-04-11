5b78b273e00aa8aecc09dcc9c5d96f1186e2113d
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-11 17:32:16 +0200

    styling

--- client/src/components/scroll-button/scroll-button.jsx
+++ client/src/components/scroll-button/scroll-button.jsx
@@ -3,5 +3,12 @@ import { Button } from '../index.js';
 import './scroll-button.css';
 
 export default function ScrollButton() {
-  return <Button iconType="arrow" iconSize="30px" />;
+  return (
+    <Button
+      iconType="top-arrow"
+      iconSize="30px"
+      iconFill="#fff2f2"
+      className="scroll-button"
+    />
+  );
 }
