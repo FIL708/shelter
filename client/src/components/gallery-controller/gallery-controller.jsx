4516ba451b4315f5592569697269bacc39986638
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-12 11:39:40 +0200

    styling

--- client/src/components/gallery-controller/gallery-controller.jsx
+++ client/src/components/gallery-controller/gallery-controller.jsx
@@ -5,32 +5,30 @@ export default function GalleryController({ value, onChange }) {
   return (
     <nav className="gallery-controller">
       <h3 className="gallery-controller__header">Tags:</h3>
-      <fieldset className="gallery-controller__fieldset">
-        <RadioButton
-          name="tags"
-          value="all"
-          checked={value === 'all'}
-          onChange={(event) => onChange(event)}
-        />
-        <RadioButton
-          name="tags"
-          value="dogs"
-          checked={value === 'dogs'}
-          onChange={(event) => onChange(event)}
-        />
-        <RadioButton
-          name="tags"
-          value="cats"
-          checked={value === 'cats'}
-          onChange={(event) => onChange(event)}
-        />
-        <RadioButton
-          name="tags"
-          value="events"
-          checked={value === 'events'}
-          onChange={(event) => onChange(event)}
-        />
-      </fieldset>
+      <RadioButton
+        name="tags"
+        value="all"
+        checked={value === 'all'}
+        onChange={(event) => onChange(event)}
+      />
+      <RadioButton
+        name="tags"
+        value="dogs"
+        checked={value === 'dogs'}
+        onChange={(event) => onChange(event)}
+      />
+      <RadioButton
+        name="tags"
+        value="cats"
+        checked={value === 'cats'}
+        onChange={(event) => onChange(event)}
+      />
+      <RadioButton
+        name="tags"
+        value="events"
+        checked={value === 'events'}
+        onChange={(event) => onChange(event)}
+      />
     </nav>
   );
 }
