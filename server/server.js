32bb3ec78035197d7386d20609235f3a04e2d93e
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-12 12:43:44 +0200

    add helmet

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const helmet = require('helmet');
 const routes = require('./routes');
 
 express()
   .use(express.json())
+  .use(helmet())
   .use('/api', routes)
   .listen(3500, () => console.log('Server started at: 3500'));
