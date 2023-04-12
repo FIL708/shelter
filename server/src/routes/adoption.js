e8fefef027ae1025bae703b974527c41201927de
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-12 13:42:23 +0200

    create adoption route

--- server/src/routes/adoption.js
+++ server/src/routes/adoption.js
@@ -8,5 +8,16 @@ const getAllAdoptions = (req, res) => {
     res.status(500).json({ message: 'error', error });
   }
 };
+const getOneAdoption = (req, res) => {
+  try {
+    const [adoption] = pets.filter((pet) => pet.id === req.params.id);
+    if (!adoption) {
+      return res.status(404).json({ message: 'Adoption not found!' });
+    }
+    res.status(200).json(adoption);
+  } catch (error) {
+    res.status(500).json({ message: 'error', error });
+  }
+};
 
-module.exports = Router().get('/', getAllAdoptions);
+module.exports = Router().get('/', getAllAdoptions).get('/:id', getOneAdoption);
