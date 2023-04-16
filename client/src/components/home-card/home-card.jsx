927b98d0e595ca8eba32e4f214f03bc1678e5c9e
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-16 20:28:05 +0200

    pass props

--- client/src/components/home-card/home-card.jsx
+++ client/src/components/home-card/home-card.jsx
@@ -2,11 +2,11 @@ import { Subtitle } from '../index.js';
 
 import './home-card.css';
 
-export default function HomeCard({ url, title, text, vertical }) {
+export default function HomeCard({ photo, title, text, vertical }) {
   const cardClassName = vertical ? 'home-card vertical' : 'home-card';
   return (
     <li className={cardClassName}>
-      <img src={url} alt={title} className="home-card__photo" />
+      <img src={photo} alt={title} className="home-card__photo" />
       <div className="home-card__content">
         <Subtitle text={title} />
         <p className="home-card__text">{text}</p>
--- client/src/components/home__goals-section/home__goals-section.jsx
+++ client/src/components/home__goals-section/home__goals-section.jsx
@@ -1,12 +1,30 @@
 import { HomeCard } from '../index.js';
 import './home__goals-section.css';
+import adoptions from './assets/adoptions.png';
+import donations from './assets/donations.png';
+import food from './assets/food.png';
 
 export default function HomeGoalsSection() {
   return (
     <section className="home__goals-section">
-      <HomeCard title="adoption" />
-      <HomeCard title="donations" />
-      <HomeCard title="food" />
+      <HomeCard
+        title="adoption"
+        photo={adoptions}
+        text="Lorem ipsum dolor sit amet consectetur. Netus integer bibendum at pharetra leo nisl."
+        vertical
+      />
+      <HomeCard
+        title="donations"
+        photo={donations}
+        text="Lorem ipsum dolor sit amet consectetur. Netus integer bibendum at pharetra leo nisl."
+        vertical
+      />
+      <HomeCard
+        title="food"
+        photo={food}
+        text="Lorem ipsum dolor sit amet consectetur. Netus integer bibendum at pharetra leo nisl."
+        vertical
+      />
     </section>
   );
 }
