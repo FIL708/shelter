3878c8ac03879152b0e7106ee6f298da1758e27b
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-16 20:47:29 +0200

    create mission section

---
+++ client/src/components/home__mission-section/home__mission-section.jsx
@@ -0,0 +1,14 @@
+import { HomeCard } from '../index.js';
+import './home__mission-section.css';
+
+export default function HomeMissionSection() {
+  return (
+    <ul className="home__mission-section">
+      <HomeCard />
+      <HomeCard />
+      <HomeCard />
+      <HomeCard />
+      <HomeCard />
+    </ul>
+  );
+}
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,21 @@
-import { Page, HomeWelcomeSection, HomeGoalsSection } from '../components';
+import {
+  Page,
+  ScrollButton,
+  Subtitle,
+  HomeWelcomeSection,
+  HomeGoalsSection,
+} from '../components';
+import { useScrollToggle } from '../hooks';
 
 export default function Home() {
+  const isScrollButtonVisible = useScrollToggle(200);
+
   return (
     <Page>
       <HomeWelcomeSection />
       <HomeGoalsSection />
+      <Subtitle text="our mission" />
+      <ScrollButton visible={isScrollButtonVisible} />
     </Page>
   );
 }
