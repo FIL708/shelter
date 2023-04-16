a9b2ecf64878f641e7f222512164274835cf82a2
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-16 20:11:45 +0200

    add vertical condition

--- client/src/components/home-card/home-card.jsx
+++ client/src/components/home-card/home-card.jsx
@@ -2,8 +2,8 @@ import { Subtitle } from '../index.js';
 
 import './home-card.css';
 
-export default function HomeCard({ url, title, text, horizontal }) {
-  const cardClassName = horizontal ? 'home-card horizontal' : 'home-card';
+export default function HomeCard({ url, title, text, vertical }) {
+  const cardClassName = vertical ? 'home-card vertical' : 'home-card';
   return (
     <li className={cardClassName}>
       <img src={url} alt={title} className="home-card__photo" />
