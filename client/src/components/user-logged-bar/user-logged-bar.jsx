a2052b7275abf2631f0a973d22fdc44a6214b5eb
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-17 12:35:46 +0200

    bar styling

--- client/src/components/user-logged-bar/user-logged-bar.jsx
+++ client/src/components/user-logged-bar/user-logged-bar.jsx
@@ -1,12 +1,17 @@
 import { Button } from '../index.js';
 import './user-logged-bar.css';
 
-export default function UserLoggedBar({ photo, role, email }) {
+export default function UserLoggedBar({ photo, email }) {
   return (
     <div className="user-logged-bar">
       <img src={photo} alt="user avatar" className="user-logged-bar__avatar" />
-      {`${role}: ${email}`}
-      <Button />
+      {email}
+      <Button
+        className="user-logged-bar__option-button"
+        iconType="option"
+        iconFill="#7286d3"
+        iconSize="18px"
+      />
     </div>
   );
 }
