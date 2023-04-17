5f41673ee832425077c74366b48ccb52329096b3
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-17 16:44:50 +0200

    pass props to dropdowns

--- client/src/components/users-filter/users-filter.jsx
+++ client/src/components/users-filter/users-filter.jsx
@@ -2,12 +2,31 @@ import { Dropdown, Textfield, Button } from '../index.js';
 import './users-filter.css';
 
 export default function UsersFilter() {
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+  };
   return (
     <form className="users-filter">
-      <Dropdown />
-      <Dropdown />
+      <Dropdown
+        label="Users per page"
+        name="usersNumber"
+        values={[10, 15, 20, 30, 40]}
+        onChange={(event) => onChange(event)}
+      />
+      <Dropdown
+        label="Sort by"
+        name="sortBy"
+        values={['newest', 'oldest', 'role', 'A-Z', 'Z-A']}
+        onChange={(event) => onChange(event)}
+      />
       <Textfield />
-      <Dropdown />
+      <Dropdown
+        label="Search by"
+        name="searchBy"
+        values={['first name', 'last name', 'email']}
+        onChange={(event) => onChange(event)}
+      />
       <Button />
     </form>
   );
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,9 +1,10 @@
-import { Page, Subtitle } from '../components';
+import { Page, Subtitle, UsersFilter } from '../components';
 
 export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
+      <UsersFilter />
     </Page>
   );
 }
