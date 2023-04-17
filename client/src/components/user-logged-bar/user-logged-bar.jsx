2367f98eb53c3aec49ed9890928a82dfcd0b1f80
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-17 15:31:43 +0200

    add default photo

--- client/src/components/user-logged-bar/user-logged-bar.jsx
+++ client/src/components/user-logged-bar/user-logged-bar.jsx
@@ -5,15 +5,18 @@ import './user-logged-bar.css';
 export default function UserLoggedBar({ photo, email, role, logoutHandler }) {
   const [isNavVisible, setIsNavVisible] = useState(false);
   const toggleNavVisibility = () => setIsNavVisible((prev) => !prev);
+  const avatar =
+    photo ||
+    'https://tleliteracy.com/wp-content/uploads/2017/02/default-avatar.png';
   return (
     <div className="user-logged-bar">
-      <img src={photo} alt="user avatar" className="user-logged-bar__avatar" />
+      <img src={avatar} alt="user avatar" className="user-logged-bar__avatar" />
       {email}
       <Button
         className="user-logged-bar__option-button"
         iconType="option"
         iconFill="#7286d3"
-        iconSize="18px"
+        iconSize="16px"
         onClick={toggleNavVisibility}
       />
       <UserNavbar
