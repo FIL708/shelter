5604690543321579d222c4be5b360fda438c2db4
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-17 11:13:54 +0200

    add context and working on structure

--- client/src/components/user-logged-bar/user-logged-bar.jsx
+++ client/src/components/user-logged-bar/user-logged-bar.jsx
@@ -1,10 +1,12 @@
-import { useContext } from 'react';
-import { UserContext } from '../..';
+import { Button } from '../index.js';
 import './user-logged-bar.css';
 
-export default function UserLoggedBar() {
-  const user = useContext(UserContext);
-  console.log(user);
-
-  return <div>user-loged-bar</div>;
+export default function UserLoggedBar({ photo, role, email }) {
+  return (
+    <div className="user-logged-bar">
+      <img src={photo} alt="user avatar" className="user-logged-bar__avatar" />
+      {`${role}: ${email}`}
+      <Button />
+    </div>
+  );
 }
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -78,7 +78,14 @@ export default function Test() {
       <HomeGoalsSection />
       <UserNavbar userRole="user" />
       <UserNavbar userRole="admin" logoutHandler={logoutHandler} />
-      <UserLoggedBar />
+      <UserLoggedBar
+        {...{
+          email: 'user.example.mail@example.com',
+          role: 'admin',
+          photo:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+        }}
+      />
     </Page>
   );
 }
