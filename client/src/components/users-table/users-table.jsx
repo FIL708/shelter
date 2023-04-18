5c10f569347be081aaaaeed307584b08bdac96d1
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-18 13:25:22 +0200

    styling header

--- client/src/components/users-table/users-table.jsx
+++ client/src/components/users-table/users-table.jsx
@@ -4,18 +4,28 @@ import './users-table.css';
 export default function UsersTable() {
   return (
     <table className="users-table">
-      <thead>
-        <tr className="users-table__row">
-          <th className="users-table__header">ID</th>
-          <th className="users-table__header">First Name</th>
-          <th className="users-table__header">Last Name</th>
-          <th className="users-table__header">Email</th>
-          <th className="users-table__header">Role</th>
-          <th className="users-table__header">Created at</th>
-          <th className="users-table__table__header">Details</th>
+      <thead className="users-table__header">
+        <tr className="users-table__header-row">
+          <th className="users-table__item">ID</th>
+          <th className="users-table__item">First Name</th>
+          <th className="users-table__item">Last Name</th>
+          <th className="users-table__item">Email</th>
+          <th className="users-table__item">Role</th>
+          <th className="users-table__item">Created at</th>
+          <th className="users-table__item">Details</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="users-table__body">
+        <UsersTableRecord
+          {...{
+            id: '1',
+            firstName: 'Joe',
+            lastName: 'Doe',
+            email: 'joedoe@example.com',
+            role: 'user',
+            createdAt: '2023-03-28 07:19:53.602 +00:00',
+          }}
+        />
         <UsersTableRecord
           {...{
             id: '1',
