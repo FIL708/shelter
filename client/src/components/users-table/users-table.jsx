fb5c9d36339ceeb34e67bcd4351809822d000fad
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-18 12:42:38 +0200

    pass props to record

--- client/src/components/users-table/users-table.jsx
+++ client/src/components/users-table/users-table.jsx
@@ -4,16 +4,29 @@ import './users-table.css';
 export default function UsersTable() {
   return (
     <table className="users-table">
-      <tr className="users-table__row">
-        <th className="users-table__row">ID</th>
-        <th className="users-table__row">First Name</th>
-        <th className="users-table__row">Last Name</th>
-        <th className="users-table__row">Email</th>
-        <th className="users-table__row">Role</th>
-        <th className="users-table__row">Created at</th>
-        <th className="users-table__row">Details</th>
-      </tr>
-      <UsersTableRecord />
+      <thead>
+        <tr className="users-table__row">
+          <th className="users-table__row">ID</th>
+          <th className="users-table__row">First Name</th>
+          <th className="users-table__row">Last Name</th>
+          <th className="users-table__row">Email</th>
+          <th className="users-table__row">Role</th>
+          <th className="users-table__row">Created at</th>
+          <th className="users-table__row">Details</th>
+        </tr>
+      </thead>
+      <tbody>
+        <UsersTableRecord
+          {...{
+            id: '1',
+            firstName: 'Joe',
+            lastName: 'Doe',
+            email: 'joedoe@example.com',
+            role: 'user',
+            createdAt: '2023-03-28 07:19:53.602 +00:00',
+          }}
+        />
+      </tbody>
     </table>
   );
 }
--- client/src/components/users-table/users-table__record.jsx
+++ client/src/components/users-table/users-table__record.jsx
@@ -1,7 +1,21 @@
-export default function UsersTableRecord() {
+export default function UsersTableRecord({
+  id,
+  firstName,
+  lastName,
+  email,
+  role,
+  createdAt,
+}) {
+  console.log(id, firstName, lastName, email, role, createdAt);
+
   return (
     <tr className="users-table__row">
-      <td>asdasd</td>
+      <td>{id}</td>
+      <td>{firstName}</td>
+      <td>{lastName}</td>
+      <td>{email}</td>
+      <td>{role}</td>
+      <td>{createdAt}</td>
     </tr>
   );
 }
