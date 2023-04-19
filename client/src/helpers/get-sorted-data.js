31a5db946f1524ad3a8f038b323a6302db95d81c
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-19 08:58:22 +0200

    add oldest and role condition

--- client/src/helpers/get-sorted-data.js
+++ client/src/helpers/get-sorted-data.js
@@ -2,6 +2,12 @@ export default function getSortedData(data, filter) {
   if (filter === 'newest') {
     return data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   }
+  if (filter === 'oldest') {
+    return data.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+  }
+  if (filter === 'role') {
+    return data.sort((a, b) => a.role.localeCompare(b.role));
+  }
 
   return data;
 }
