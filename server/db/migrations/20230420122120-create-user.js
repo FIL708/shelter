3d14a7a4cdafcde55d5e8ce1af51d2a7fa32cd29
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-20 16:37:11 +0200

    add seed and password filed in model and migration

--- server/db/migrations/20230420122120-create-user.js
+++ server/db/migrations/20230420122120-create-user.js
@@ -20,6 +20,10 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
