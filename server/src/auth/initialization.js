bea1ee409f5e38942523a74ddcd2a9dace6f797d
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-21 14:20:55 +0200

    craete auth func for local strategy

--- server/src/auth/initialization.js
+++ server/src/auth/initialization.js
@@ -1,10 +1,23 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
 const config = require('config');
+const { User } = require('../models');
 
 function passportInitialization() {
   console.log(LocalStrategy, config);
+
+  const authUser = async (email, password, done) => {
+    const user = await User.findOne({ where: { email } });
+    if (!user) return done(null, false, user);
+
+    if (user.password === password) {
+      return done(null, user);
+    }
+    return done(null, false, user);
+  };
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }), authUser);
 
   passport.serializeUser((user, done) => done(null, user));
   passport.deSerializeUser((user, done) => done(null, user));
