8531520d14bb312396ac9d89ff041ceb7c38b245
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-22 15:11:06 +0200

    fix headers bug

--- server/src/routes/status.js
+++ server/src/routes/status.js
@@ -1,7 +1,6 @@
 module.exports = function (req, res) {
-  console.log(req.session.passport);
   if (req.session.passport) {
-    res.status(200).json(req.session.passport.user);
+    return res.status(200).json({ user: req.session.passport.user });
   }
   return res.status(200).json({ message: 'Not-logged' });
 };
