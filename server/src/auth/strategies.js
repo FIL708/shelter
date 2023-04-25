1546f9a4ae072c941a6b442ff3aa3e18594f8199
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-25 11:56:11 +0200

    end up with google strategy

--- server/src/auth/strategies.js
+++ server/src/auth/strategies.js
@@ -1,6 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const bcrypt = require('bcrypt');
+const passwordGenerator = require('generate-password');
 const config = require('config');
 const { User } = require('../models');
 
@@ -8,7 +9,7 @@ const serverUrl = config.get('serverUrl');
 const googleClientID = config.get('googleClientID');
 const googleClientSecret = config.get('googleClientSecret');
 
-const localAuth = async (email, password, done) => {
+const localVerify = async (email, password, done) => {
   try {
     const user = await User.findOne({ where: { email } });
     const isValidPassword = await bcrypt.compare(password, user.password);
@@ -23,24 +24,38 @@ const localAuth = async (email, password, done) => {
   }
 };
 
-const googleAuth = (accessToken, refreshToken, profile, done) => {
+const googleVerify = async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
     const avatar = profile.photos[0].value;
-    console.log(email, avatar);
 
-    const user = User.findOne({ where: { email } });
-    if (user) {
-      return done(false, user);
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      const password = passwordGenerator.generate({
+        length: 12,
+        numbers: true,
+      });
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log(`Sending mail with password to user: ${password}`);
+
+      const newUser = await User.create({
+        email,
+        avatar,
+        password: hashedPassword,
+      });
+      return done(false, newUser);
     }
 
-    return done(false, profile);
+    return done(false, user);
   } catch (error) {
     return done(error);
   }
 };
 
-const localStrategy = new LocalStrategy({ usernameField: 'email' }, localAuth);
+const localStrategy = new LocalStrategy(
+  { usernameField: 'email' },
+  localVerify,
+);
 
 const googleStrategy = new GoogleStrategy(
   {
@@ -48,7 +63,7 @@ const googleStrategy = new GoogleStrategy(
     clientSecret: googleClientSecret,
     callbackURL: `${serverUrl}/api/auth/google/redirect`,
   },
-  googleAuth,
+  googleVerify,
 );
 
 module.exports = { localStrategy, googleStrategy };
