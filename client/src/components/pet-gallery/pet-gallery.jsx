3c1d231ccb75aaa2d018164c59a3e507e9a1a90a
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-26 14:12:31 +0200

    fix layout styling

--- client/src/components/pet-gallery/pet-gallery.jsx
+++ client/src/components/pet-gallery/pet-gallery.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Button } from '../index.js';
 import PetGalleryList from './pet-gallery__list/pet-gallery__list.jsx';
 import './pet-gallery.css';
 
@@ -11,10 +12,22 @@ export default function PetGallery({ gallery }) {
   return (
     <section className="pet-gallery">
       <div className="pet-gallery__main">
+        <Button
+          iconType="arrow"
+          iconFill="#7286d3"
+          iconSize="40px"
+          className="pet-gallery__button left"
+        />
         <img
           src={mainPhoto}
           alt="pet gallery main"
           className="pet-gallery__main-photo"
+        />
+        <Button
+          iconType="arrow"
+          iconFill="#7286d3"
+          iconSize="40px"
+          className="pet-gallery__button right"
         />
       </div>
       <PetGalleryList
