bb8398b1a56d8920ae24b6765973e9cbe0da17d7
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-26 13:08:13 +0200

    styling list

--- client/src/components/pet-gallery/pet-gallery__list/pet-gallery__list.jsx
+++ client/src/components/pet-gallery/pet-gallery__list/pet-gallery__list.jsx
@@ -4,8 +4,12 @@ export default function PetGalleryList({ gallery }) {
   return (
     <ul className="pet-gallery__list">
       {gallery.map((photo) => (
-        <li className="pet-gallery__list-item">
-          <img src={photo} alt="pet gallery list item" />
+        <li className="pet-gallery__list-item" key={photo}>
+          <img
+            src={photo}
+            alt="pet gallery list item"
+            className="pet-gallery__list-item__photo"
+          />
         </li>
       ))}
     </ul>
