507dee7777086c82008d64df8e371d41674f7396
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-26 15:05:55 +0200

    add prev and next photo button

--- client/src/components/pet-gallery/pet-gallery.jsx
+++ client/src/components/pet-gallery/pet-gallery.jsx
@@ -8,16 +8,43 @@ export default function PetGallery({ gallery }) {
 
   const setPhotoAsMain = (url) => setMainPhoto(url);
 
+  const setNextPhoto = () => {
+    const index = gallery.indexOf(mainPhoto);
+    if (index + 2 > gallery.length) {
+      setMainPhoto(gallery[0]);
+    } else {
+      setMainPhoto(gallery[index + 1]);
+    }
+  };
+  const setPreviousPhoto = () => {
+    const index = gallery.indexOf(mainPhoto);
+    if (index - 1 < 0) {
+      setMainPhoto(gallery[gallery.length - 1]);
+    } else {
+      setMainPhoto(gallery[index - 1]);
+    }
+  };
+
   return (
     <section className="pet-gallery">
       <div className="pet-gallery__main">
-        <Button iconType="arrow-left" iconFill="#7286d3" iconSize="35px" />
+        <Button
+          iconType="arrow-left"
+          iconFill="#7286d3"
+          iconSize="35px"
+          onClick={setPreviousPhoto}
+        />
         <img
           src={mainPhoto}
           alt="pet gallery main"
           className="pet-gallery__main-photo"
         />
-        <Button iconType="arrow-right" iconFill="#7286d3" iconSize="35px" />
+        <Button
+          iconType="arrow-right"
+          iconFill="#7286d3"
+          iconSize="35px"
+          onClick={setNextPhoto}
+        />
       </div>
       <PetGalleryList
         gallery={gallery}
