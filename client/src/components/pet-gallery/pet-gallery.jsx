086e6a9922a50378dcce0244eaa757727e8cfbb0
FIL708 <filip.zebrowski4013@gmail.com>
2023-04-26 13:15:50 +0200

    add conditional classname

--- client/src/components/pet-gallery/pet-gallery.jsx
+++ client/src/components/pet-gallery/pet-gallery.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 import PetGalleryList from './pet-gallery__list/pet-gallery__list.jsx';
 import './pet-gallery.css';
 
 export default function PetGallery({ gallery }) {
+  const [mainPhoto, setMainPhoto] = useState(gallery[0]);
+  console.log(setMainPhoto);
+
   return (
     <section className="pet-gallery">
       <div className="pet-gallery__main">
         <img
-          src={gallery[0]}
+          src={mainPhoto}
           alt="pet gallery main"
           className="pet-gallery__main-photo"
         />
       </div>
-      <PetGalleryList gallery={gallery} />
+      <PetGalleryList gallery={gallery} main={mainPhoto} />
     </section>
   );
 }
--- client/src/components/pet-gallery/pet-gallery__list/pet-gallery__list.jsx
+++ client/src/components/pet-gallery/pet-gallery__list/pet-gallery__list.jsx
@@ -1,17 +1,23 @@
 import './pet-gallery__list.css';
 
-export default function PetGalleryList({ gallery }) {
+export default function PetGalleryList({ gallery, main }) {
   return (
     <ul className="pet-gallery__list">
-      {gallery.map((photo) => (
-        <li className="pet-gallery__list-item" key={photo}>
-          <img
-            src={photo}
-            alt="pet gallery list item"
-            className="pet-gallery__list-item__photo"
-          />
-        </li>
-      ))}
+      {gallery.map((photo) => {
+        const className =
+          photo === main
+            ? 'pet-gallery__list-item main-photo'
+            : 'pet-gallery__list-item';
+        return (
+          <li className={className} key={photo}>
+            <img
+              src={photo}
+              alt="pet gallery list item"
+              className="pet-gallery__list-item__photo"
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 }
