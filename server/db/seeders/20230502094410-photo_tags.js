6691463eaf6779f0aa2dd05e9ee3d74455111522
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-02 13:57:28 +0200

    add photo-tag seeders

--- server/db/seeders/20230502094410-photo_tags.js
+++ server/db/seeders/20230502094410-photo_tags.js
@@ -1,25 +1,20 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface) {
+    return queryInterface.bulkInsert('photo_tags', [
+      { photoID: 1, tagID: 1 },
+      { photoID: 6, tagID: 1 },
+      { photoID: 11, tagID: 1 },
+      { photoID: 16, tagID: 1 },
+      { photoID: 21, tagID: 1 },
+      { photoID: 26, tagID: 1 },
+      { photoID: 31, tagID: 1 },
+      { photoID: 36, tagID: 1 },
+      { photoID: 41, tagID: 1 },
+      { photoID: 46, tagID: 1 },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface) {
+    queryInterface.bulkDelete('photo_tags', null, {});
+  },
 };
