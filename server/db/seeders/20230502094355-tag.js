e718f8ac041e0f8393afc07207d744e26ab1771b
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-02 13:50:45 +0200

    add tag seeders

--- server/db/seeders/20230502094355-tag.js
+++ server/db/seeders/20230502094355-tag.js
@@ -1,25 +1,14 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface) {
+    return queryInterface.bulkInsert('Tags', [
+      { name: 'main' },
+      { name: 'cat' },
+      { name: 'dog' },
+      { name: 'event' },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface) {
+    queryInterface.bulkDelete('Tags', null, {});
+  },
 };
