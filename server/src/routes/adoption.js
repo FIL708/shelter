059dbdd1af2dfdfba8b367951e48ac4108e021d4
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-05 13:03:47 +0200

    add route for one adoption

--- server/src/routes/adoption.js
+++ server/src/routes/adoption.js
@@ -2,14 +2,36 @@ const { Router } = require('express');
 const { Adoption, Photo } = require('../models');
 
 const getAllAdoptions = async (req, res) => {
-  const adoptions = await Adoption.findAll({
-    attributes: { exclude: ['adoptionId'] },
-    include: [{ model: Photo, as: 'photos' }],
-  });
-  return res.status(200).json(adoptions);
+  try {
+    const adoptions = await Adoption.findAll({
+      attributes: { exclude: ['adoptionId'] },
+      include: [{ model: Photo, as: 'photos' }],
+    });
+
+    if (!adoptions) {
+      return res.status(404).json({ message: 'Adoptions not found' });
+    }
+
+    return res.status(200).json(adoptions);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
-const getOneAdoption = (req, res) => {
-  console.log(Adoption, req, res);
+const getOneAdoption = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const adoption = await Adoption.findByPk(id, {
+      attributes: { exclude: ['adoptionId'] },
+      include: [{ model: Photo, as: 'photos' }],
+    });
+
+    if (!adoption) {
+      return res.status(404).json({ message: 'Adoption not found' });
+    }
+    return res.status(200).json(adoption);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 module.exports = Router().get('/', getAllAdoptions).get('/:id', getOneAdoption);
