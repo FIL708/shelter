ba1d736dd54a0fe088396769957b24378d2ed14b
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-05 15:49:06 +0200

    enhance formatted function functiontallity

--- client/src/helpers/get-formatted-date.js
+++ client/src/helpers/get-formatted-date.js
@@ -1,9 +1,20 @@
-export default function getFormattedDate(date) {
+export default function getFormattedDate(date, setting) {
   const rawDate = new Date(date);
-  const options = {
-    day: '2-digit',
-    month: 'long',
-    year: 'numeric',
-  };
+
+  const options =
+    setting === 'full'
+      ? {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+        }
+      : {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        };
+
   return rawDate.toLocaleString([], options);
 }
