fa8ae08701207d2b156ea4a1f5fc4c634003b534
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-06 13:20:06 +0200

    fix react routing

--- client/src/components/pet-card/pet-card-photo/pet-card-photo.jsx
+++ client/src/components/pet-card/pet-card-photo/pet-card-photo.jsx
@@ -4,7 +4,7 @@ import './pet-card-photo.css';
 export default function PetCardPhoto({ url, mode, id }) {
   const linkClassName = mode ? `pet-card__link mode` : 'pet-card__link ';
   return (
-    <Link to={`/adoption/${id}`} className={linkClassName}>
+    <Link to={`/adoptions/${id}`} className={linkClassName}>
       <img src={url} alt="pet" className="pet-card-photo" />
       <p className="pet-card__hover-text">Click me!</p>
     </Link>
