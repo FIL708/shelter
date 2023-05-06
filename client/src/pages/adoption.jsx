4d1c9800a48cbe1e15f42aa7050ab5f4fc38b8de
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-06 16:35:52 +0200

    clean up the code

--- client/src/pages/adoption.jsx
+++ client/src/pages/adoption.jsx
@@ -2,24 +2,19 @@ import { useParams } from 'react-router-dom';
 import {
   Page,
   Subtitle,
-  // PetGallery,
-  // ScrollButton,
-  // PetDetails,
-  // PetDescription,
+  PetGallery,
+  ScrollButton,
+  PetDetails,
+  PetDescription,
   LoadingSpinner,
   ErrorCard,
 } from '../components';
-import {
-  // useScrollToggle,
-  useFetch,
-} from '../hooks';
+import { useScrollToggle, useFetch } from '../hooks';
 
 export default function Adoption() {
   const { id } = useParams();
   const [pet, isLoading, error] = useFetch(`/api/adoption/${id}`);
-  console.log(pet, isLoading, error);
-
-  // const isScrollButtonVisible = useScrollToggle(200);
+  const isScrollButtonVisible = useScrollToggle(200);
 
   if (isLoading || error)
     return (
@@ -35,13 +30,13 @@ export default function Adoption() {
 
   return (
     <Page>
-      {/* <Subtitle main text={`Adoption no. ${id}`} />
+      <Subtitle main text={`Adoption no. ${id}`} />
       <PetDetails data={pet} />
       <PetGallery photos={pet.photos} />
       <Subtitle text="Something About Me" />
       <PetDescription text={pet.description} />
       <Subtitle text="What people think about me" />
-      <ScrollButton visible={isScrollButtonVisible} /> */}
+      <ScrollButton visible={isScrollButtonVisible} />
     </Page>
   );
 }
