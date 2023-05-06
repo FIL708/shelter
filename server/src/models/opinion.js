16467ca6d3573cb9faca603ef342d21eddbe85c4
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-06 18:35:23 +0200

    add association to opinion

--- server/src/models/opinion.js
+++ server/src/models/opinion.js
@@ -2,7 +2,9 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Opinion extends Model {
-    static associate(models) {}
+    static associate(models) {
+      Opinion.belongsTo(models.Adoption, { foreignKey: 'adoptionId' });
+    }
   }
   Opinion.init(
     {
