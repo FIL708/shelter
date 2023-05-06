28865a9e0bde1e3b16be8b0bf06871ab1e9c2aea
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-06 16:15:27 +0200

    modify photo seeders

--- server/db/seeders/20230502094348-photo.js
+++ server/db/seeders/20230502094348-photo.js
@@ -6,19 +6,19 @@ module.exports = {
         adoptionId: 1,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://cdn.galleries.smcloud.net/t/galleries/gf-3YpJ-Szag-K4Gg_dog-niemiecki-1920x1080-nocrop.jpg',
         adoptionId: 1,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://www.humanesociety.org/sites/default/files/2019/02/dog-451643.jpg',
         adoptionId: 1,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://media.wired.com/photos/636eb5510ae5a121565fd729/4:3/w_1983,h_1487,c_limit/WI110122_FF_ForeverDogs_2400x1350_crop.jpg',
         adoptionId: 1,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://th-thumbnailer.cdn-si-edu.com/SdKYWifCKfE2g8O-po_SO99hQ-Y=/1000x750/filters:no_upscale():focal(3126x2084:3127x2085)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/ec/e6/ece69181-708a-496e-b2b7-eaf7078b99e0/gettyimages-1310156391.jpg',
         adoptionId: 1,
       },
       {
@@ -26,19 +26,19 @@ module.exports = {
         adoptionId: 2,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://cdn.galleries.smcloud.net/t/galleries/gf-3YpJ-Szag-K4Gg_dog-niemiecki-1920x1080-nocrop.jpg',
         adoptionId: 2,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://www.humanesociety.org/sites/default/files/2019/02/dog-451643.jpg',
         adoptionId: 2,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://media.wired.com/photos/636eb5510ae5a121565fd729/4:3/w_1983,h_1487,c_limit/WI110122_FF_ForeverDogs_2400x1350_crop.jpg',
         adoptionId: 2,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://th-thumbnailer.cdn-si-edu.com/SdKYWifCKfE2g8O-po_SO99hQ-Y=/1000x750/filters:no_upscale():focal(3126x2084:3127x2085)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/ec/e6/ece69181-708a-496e-b2b7-eaf7078b99e0/gettyimages-1310156391.jpg',
         adoptionId: 2,
       },
       {
@@ -46,19 +46,19 @@ module.exports = {
         adoptionId: 3,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://cdn.galleries.smcloud.net/t/galleries/gf-3YpJ-Szag-K4Gg_dog-niemiecki-1920x1080-nocrop.jpg',
         adoptionId: 3,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://www.humanesociety.org/sites/default/files/2019/02/dog-451643.jpg',
         adoptionId: 3,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://media.wired.com/photos/636eb5510ae5a121565fd729/4:3/w_1983,h_1487,c_limit/WI110122_FF_ForeverDogs_2400x1350_crop.jpg',
         adoptionId: 3,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://th-thumbnailer.cdn-si-edu.com/SdKYWifCKfE2g8O-po_SO99hQ-Y=/1000x750/filters:no_upscale():focal(3126x2084:3127x2085)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/ec/e6/ece69181-708a-496e-b2b7-eaf7078b99e0/gettyimages-1310156391.jpg',
         adoptionId: 3,
       },
       {
@@ -66,19 +66,19 @@ module.exports = {
         adoptionId: 4,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://cdn.galleries.smcloud.net/t/galleries/gf-3YpJ-Szag-K4Gg_dog-niemiecki-1920x1080-nocrop.jpg',
         adoptionId: 4,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://www.humanesociety.org/sites/default/files/2019/02/dog-451643.jpg',
         adoptionId: 4,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://media.wired.com/photos/636eb5510ae5a121565fd729/4:3/w_1983,h_1487,c_limit/WI110122_FF_ForeverDogs_2400x1350_crop.jpg',
         adoptionId: 4,
       },
       {
-        url: 'https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg',
+        url: 'https://th-thumbnailer.cdn-si-edu.com/SdKYWifCKfE2g8O-po_SO99hQ-Y=/1000x750/filters:no_upscale():focal(3126x2084:3127x2085)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/ec/e6/ece69181-708a-496e-b2b7-eaf7078b99e0/gettyimages-1310156391.jpg',
         adoptionId: 4,
       },
       {
@@ -86,19 +86,19 @@ module.exports = {
         adoptionId: 5,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg',
         adoptionId: 5,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://www.rd.com/wp-content/uploads/2019/09/Cute-cat-lying-on-his-back-on-the-carpet.-Breed-British-mackerel-with-yellow-eyes-and-a-bushy-mustache.-Close-up-e1573490045672.jpg',
         adoptionId: 5,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://i.insider.com/63992b55b08ee10018f4d917?width=700',
         adoptionId: 5,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://allkindsvet.com/wp-content/uploads/2022/03/cat-meowing.jpg',
         adoptionId: 5,
       },
       {
@@ -106,19 +106,19 @@ module.exports = {
         adoptionId: 6,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg',
         adoptionId: 6,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://www.rd.com/wp-content/uploads/2019/09/Cute-cat-lying-on-his-back-on-the-carpet.-Breed-British-mackerel-with-yellow-eyes-and-a-bushy-mustache.-Close-up-e1573490045672.jpg',
         adoptionId: 6,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://i.insider.com/63992b55b08ee10018f4d917?width=700',
         adoptionId: 6,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://allkindsvet.com/wp-content/uploads/2022/03/cat-meowing.jpg',
         adoptionId: 6,
       },
       {
@@ -126,19 +126,19 @@ module.exports = {
         adoptionId: 7,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg',
         adoptionId: 7,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://www.rd.com/wp-content/uploads/2019/09/Cute-cat-lying-on-his-back-on-the-carpet.-Breed-British-mackerel-with-yellow-eyes-and-a-bushy-mustache.-Close-up-e1573490045672.jpg',
         adoptionId: 7,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://i.insider.com/63992b55b08ee10018f4d917?width=700',
         adoptionId: 7,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://allkindsvet.com/wp-content/uploads/2022/03/cat-meowing.jpg',
         adoptionId: 7,
       },
       {
@@ -146,19 +146,19 @@ module.exports = {
         adoptionId: 8,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg',
         adoptionId: 8,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://www.rd.com/wp-content/uploads/2019/09/Cute-cat-lying-on-his-back-on-the-carpet.-Breed-British-mackerel-with-yellow-eyes-and-a-bushy-mustache.-Close-up-e1573490045672.jpg',
         adoptionId: 8,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://i.insider.com/63992b55b08ee10018f4d917?width=700',
         adoptionId: 8,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://allkindsvet.com/wp-content/uploads/2022/03/cat-meowing.jpg',
         adoptionId: 8,
       },
       {
@@ -166,19 +166,19 @@ module.exports = {
         adoptionId: 9,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg',
         adoptionId: 9,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://www.rd.com/wp-content/uploads/2019/09/Cute-cat-lying-on-his-back-on-the-carpet.-Breed-British-mackerel-with-yellow-eyes-and-a-bushy-mustache.-Close-up-e1573490045672.jpg',
         adoptionId: 9,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://i.insider.com/63992b55b08ee10018f4d917?width=700',
         adoptionId: 9,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://allkindsvet.com/wp-content/uploads/2022/03/cat-meowing.jpg',
         adoptionId: 9,
       },
       {
@@ -186,19 +186,19 @@ module.exports = {
         adoptionId: 10,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg',
         adoptionId: 10,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://www.rd.com/wp-content/uploads/2019/09/Cute-cat-lying-on-his-back-on-the-carpet.-Breed-British-mackerel-with-yellow-eyes-and-a-bushy-mustache.-Close-up-e1573490045672.jpg',
         adoptionId: 10,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://i.insider.com/63992b55b08ee10018f4d917?width=700',
         adoptionId: 10,
       },
       {
-        url: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg',
+        url: 'https://allkindsvet.com/wp-content/uploads/2022/03/cat-meowing.jpg',
         adoptionId: 10,
       },
     ]);
