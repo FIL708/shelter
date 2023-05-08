604efa5e9e45e846df79cd93a3d7d45410429e57
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-08 20:02:56 +0200

    add new text in mession section

--- client/src/components/home__mission-section/home__mission-section.jsx
+++ client/src/components/home__mission-section/home__mission-section.jsx
@@ -12,27 +12,27 @@ export default function HomeMissionSection() {
       <HomeCard
         photo={help}
         title="help"
-        text="Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+        text="HelpMeDude! strives to improve the lives of homeless pets by connecting them with loving families and creating a compassionate community dedicated to animal welfare."
       />
       <HomeCard
         photo={care}
         title="care"
-        text="Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+        text="We take pride in providing the highest level of care and attention to each and every pet in our program, ensuring that they receive the medical attention, nutrition, and socialization they need to thrive and find their forever homes."
       />
       <HomeCard
         photo={treat}
         title="treat"
-        text="Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+        text="The health and well-being of our pets is a top priority at HelpMeDude!. We provide all necessary medical treatment, including vaccinations, spaying/neutering, and any other required procedures."
       />
       <HomeCard
         photo={feed}
         title="feed"
-        text="Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+        text="Feeding our pets a healthy and nutritious diet is essential to their overall health and well-being, which is why at HelpMeDude!, we take great care to provide our furry friends with high-quality pet food that meets their specific nutritional needs."
       />
       <HomeCard
         photo={find}
         title="find new home"
-        text="Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+        text="Our ultimate mission is to find each of our furry friends their perfect forever home, where they can receive the love, care, and attention they deserve. We believe that every pet deserves a second chance at happiness, and we work tirelessly to connect them with loving families."
       />
     </ul>
   );
