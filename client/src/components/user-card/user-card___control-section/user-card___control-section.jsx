0a04b36f630cc724c293a2bb5c8af2eec28c3f95
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-08 12:08:47 +0200

    styling control section

--- client/src/components/user-card/user-card___control-section/user-card___control-section.jsx
+++ client/src/components/user-card/user-card___control-section/user-card___control-section.jsx
@@ -2,14 +2,19 @@ import { Button } from '../../index.js';
 import './user-card___control-section.css';
 
 export default function UserCardControlSection({ userId, avatar, role }) {
-  const userRole = role === 'admin' ? `Admin: ${userId}` : `User: ${userId}`;
+  const userRole =
+    role === 'admin' ? `Admin ID: ${userId}` : `User ID: ${userId}`;
   return (
     <div className="user-card__control-section">
-      <img src={avatar} alt="user avatar" />
+      <img
+        src={avatar}
+        alt="user avatar"
+        className="user-card__control-section__avatar"
+      />
       <span className="user-card__control-section__id">{userRole}</span>
-      <Button />
-      <Button />
-      <Button />
+      <Button text="Change password" className="outline" />
+      <Button text="Update profile" className="outline" />
+      <Button text="Delete account" className="outline red" />
     </div>
   );
 }
