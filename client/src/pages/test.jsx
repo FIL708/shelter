cb916410b1c19b26f0bec775da480210c54d15ed
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-08 11:44:43 +0200

    add dummy data

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,10 +1,21 @@
 import { Page, Subtitle, UserCard } from '../components';
 
 export default function Test() {
+  const user = {
+    firstName: 'Filip',
+    lastName: 'Zebrowski',
+    role: 'admin',
+    createdAt: '2023-05-06 17:18:01.651 +00:00',
+    phone: '123 123 123',
+    address: { city: 'Wroclaw', country: 'Poland' },
+    avatar:
+      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    birthday: '1995-05-06 17:18:01.651 +00:00',
+  };
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <UserCard />
+      <UserCard userData={user} />
     </Page>
   );
 }
