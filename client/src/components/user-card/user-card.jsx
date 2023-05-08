f742910f76aa19a797bde8d529d888487f091f24
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-08 16:00:23 +0200

    change naming

--- client/src/components/user-card/user-card.jsx
+++ client/src/components/user-card/user-card.jsx
@@ -1,11 +1,11 @@
 import './user-card.css';
-import UserCardControlSection from './user-card___control-section/user-card___control-section.jsx';
+import UserCardControl from './user-card___control/user-card___control.jsx';
 import UserCardDataSection from './user-card__data-section/user-card__data-section.jsx';
 
 export default function UserCard({ userData }) {
   return (
     <section className="user-card">
-      <UserCardControlSection
+      <UserCardControl
         userId={userData.id}
         avatar={userData.avatar}
         role={userData.role}
--- client/src/components/user-card/user-card___control/user-card___control.jsx
+++ client/src/components/user-card/user-card___control/user-card___control.jsx
@@ -1,17 +1,17 @@
 import { Button } from '../../index.js';
-import './user-card___control-section.css';
+import './user-card___control.css';
 
-export default function UserCardControlSection({ userId, avatar, role }) {
+export default function UserCardControl({ userId, avatar, role }) {
   const userRole =
     role === 'admin' ? `Admin ID: ${userId}` : `User ID: ${userId}`;
   return (
-    <div className="user-card__control-section">
+    <div className="user-card__control">
       <img
         src={avatar}
         alt="user avatar"
-        className="user-card__control-section__avatar"
+        className="user-card__control__avatar"
       />
-      <span className="user-card__control-section__id">{userRole}</span>
+      <span className="user-card__control__id">{userRole}</span>
       <Button text="Change password" className="outline" />
       <Button text="Update profile" className="outline" />
       <Button text="Delete account" className="outline red" />
