67f95a50161b6a09c927b2cc898f8ef2912da9cd
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-08 15:52:14 +0200

    add loading and error catching

--- client/src/pages/user.jsx
+++ client/src/pages/user.jsx
@@ -1,9 +1,32 @@
-import { Page, Subtitle } from '../components';
+import { useParams } from 'react-router-dom';
+import {
+  ErrorCard,
+  LoadingSpinner,
+  Page,
+  Subtitle,
+  UserCard,
+} from '../components';
+import { useFetch } from '../hooks';
 
 export default function User() {
+  const { id } = useParams();
+  const [user, isLoading, error] = useFetch(`/api/user/${id}`);
+
+  if (isLoading || error)
+    return (
+      <Page>
+        <Subtitle text="Gallery" main />
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <ErrorCard errorCode={error.status} errorMessage={error.message} />
+        )}
+      </Page>
+    );
   return (
     <Page>
       <Subtitle text="Profile" main />
+      <UserCard userData={user} />
     </Page>
   );
 }
