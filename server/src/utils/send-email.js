b3a6456a9cc06f37aa80788657b8f0b19fd40ee0
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-22 15:25:58 +0200

    integrate nodemailer and plugin

--- server/src/utils/send-email.js
+++ server/src/utils/send-email.js
@@ -1,10 +1,27 @@
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
 const config = require('config');
 
 const smtpSettings = config.get('smtp');
 
 module.exports = async function sendEmail({ email, body, subject }) {
   const transporter = nodemailer.createTransport(smtpSettings);
+
+  transporter.use(
+    'compile',
+    hbs({
+      viewEngine: {
+        extname: '.hbs',
+        layoutsDir: '../templates/',
+        defaultLayout: false,
+        partialsDir: '../templates/partials/',
+      },
+      viewPath: 'templates/',
+      extName: '.hbs',
+    }),
+  );
+
   try {
     const mail = await transporter.sendMail({
       from: '"HelpMeDude! - Shelter" <helpmedude@shelter.com>',
