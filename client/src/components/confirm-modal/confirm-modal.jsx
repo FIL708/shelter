e94b39fb552c9cdfbfc6ff878078bd4d0a3cf177
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-29 14:25:34 +0200

    start adding funtionallity

--- client/src/components/confirm-modal/confirm-modal.jsx
+++ client/src/components/confirm-modal/confirm-modal.jsx
@@ -1,4 +1,4 @@
-import { Subtitle, Textfield, Button } from '../index.js';
+import { Subtitle, Textfield, Button, ModalWrapper } from '../index.js';
 import './confirm-modal.css';
 
 export default function ConfirmModal({
@@ -7,22 +7,27 @@ export default function ConfirmModal({
   textToConfirm,
   onConfirm,
   onCancel,
+  isVisible,
+  toggleModalVision,
 }) {
   return (
-    <div className="confirm-modal">
-      <Subtitle text={title} />
-      <p className="confirm-modal__question">{question}</p>
-      {textToConfirm && (
-        <Textfield
-          className="confirm-modal__input"
-          placeholder={`To confirm type: ${textToConfirm}`}
-        />
-      )}
+    <ModalWrapper isVisible={isVisible} toggleModalVision={toggleModalVision}>
+      <form className="confirm-modal">
+        <Subtitle text={title} />
+        <p className="confirm-modal__question">{question}</p>
+        {textToConfirm && (
+          <Textfield
+            className="confirm-modal__input"
+            placeholder={`To confirm type: ${textToConfirm}`}
+            required
+          />
+        )}
 
-      <div className="confirm-modal__buttons">
-        <Button text="Confirm" onClick={onConfirm} />
-        <Button className="cancel" text="Cancel" onClick={onCancel} />
-      </div>
-    </div>
+        <div className="confirm-modal__buttons">
+          <Button text="Confirm" onClick={onConfirm} />
+          <Button className="cancel" text="Cancel" onClick={onCancel} />
+        </div>
+      </form>
+    </ModalWrapper>
   );
 }
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,13 +1,26 @@
-import { Page, Subtitle, ConfirmModal } from '../components';
+import { useState } from 'react';
+import { Page, Subtitle, ConfirmModal, Button } from '../components';
 
 export default function Test() {
+  const [visibleModals, setVisibleModals] = useState({ confirm: false });
+  const onClickConfirm = () => {
+    setVisibleModals((prev) => ({ ...prev, confirm: !prev.confirm }));
+  };
+  const cancelConfirmMode = () => {
+    onClickConfirm();
+  };
+
   return (
     <Page>
       <Subtitle text="Test Page" main />
+      <Button text="click" onClick={onClickConfirm} />
       <ConfirmModal
         title="Delete account"
         textToConfirm="DELETE"
         question="Are you sure you want to delete your account?"
+        isVisible={visibleModals.confirm}
+        toggleModalVision={onClickConfirm}
+        onCancel={cancelConfirmMode}
       />
     </Page>
   );
