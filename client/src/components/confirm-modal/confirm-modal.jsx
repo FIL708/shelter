d74d6307c0bd9959ae86c9550fc7167d1e7d63e1
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-29 14:06:16 +0200

    button styling

--- client/src/components/confirm-modal/confirm-modal.jsx
+++ client/src/components/confirm-modal/confirm-modal.jsx
@@ -1,15 +1,24 @@
 import { Subtitle, Textfield, Button } from '../index.js';
 import './confirm-modal.css';
 
-export default function ConfirmModal({ title, question, onConfirm }) {
+export default function ConfirmModal({
+  title,
+  question,
+  textToConfirm,
+  onConfirm,
+  onCancel,
+}) {
   return (
     <div className="confirm-modal">
       <Subtitle text={title} />
       <p className="confirm-modal__question">{question}</p>
-      <Textfield />
+      {textToConfirm && (
+        <Textfield placeholder={`To confirm type: ${textToConfirm}`} />
+      )}
+
       <div className="confirm-modal__buttons">
         <Button text="Confirm" onClick={onConfirm} />
-        <Button text="Cancel" />
+        <Button className="cancel" text="Cancel" onClick={onCancel} />
       </div>
     </div>
   );
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -4,7 +4,11 @@ export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <ConfirmModal title="Delete account" />
+      <ConfirmModal
+        title="Delete account"
+        textToConfirm="DELETE"
+        question="Are you sure you want to delete your account?"
+      />
     </Page>
   );
 }
