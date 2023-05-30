8d563288e8fdd2472fe17267ba6874a2195f1fa0
FIL708 <filip.zebrowski4013@gmail.com>
2023-05-30 12:46:06 +0200

    add buttons

--- client/src/components/change-password-form/change-password-form.jsx
+++ client/src/components/change-password-form/change-password-form.jsx
@@ -1,12 +1,16 @@
-import { Subtitle, Textfield } from '../index.js';
+import { Subtitle, Textfield, Button } from '../index.js';
 import './change-password-form.css';
 
 export default function ChangePasswordForm() {
   return (
     <form className="change-password-form">
       <Subtitle text="Change password" />
-      <Textfield />
-      <Textfield />
+      <Textfield name="new-password" label="Password" />
+      <Textfield name="confirm-password" label="Confirm password" />
+      <div className="confirm-modal__buttons">
+        <Button text="Confirm" />
+        <Button className="cancel" text="Cancel" />
+      </div>
     </form>
   );
 }
