396c6386b59cda259d70e110c688f4b9faa62eef
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-01 17:01:46 +0200

    add components

--- client/src/components/profile-form/profile-form.jsx
+++ client/src/components/profile-form/profile-form.jsx
@@ -1,5 +1,30 @@
+import { Subtitle, Textfield, Button } from '../index.js';
 import './profile-form.css';
 
 export default function ProfileForm() {
-  return <form className="profile-form">form</form>;
+  return (
+    <form className="profile-form">
+      <Subtitle text="Update Profile" />
+      <fieldset>
+        <Textfield />
+        <Textfield />
+      </fieldset>
+      <fieldset>
+        <Textfield />
+        <Textfield />
+      </fieldset>
+      <fieldset>
+        <Textfield />
+        <Textfield />
+      </fieldset>
+      <fieldset>
+        <img src="" alt="avatar" />
+        <Textfield />
+      </fieldset>
+      <div className="confirm-modal__buttons">
+        <Button text="Confirm" />
+        <Button className="cancel" text="Cancel" />
+      </div>
+    </form>
+  );
 }
