cf83683b1a4b5a0ef93fb3087ad0d8b550c63e08
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-04 19:32:29 +0200

    add styling

--- client/src/components/profile-form/profile-form.jsx
+++ client/src/components/profile-form/profile-form.jsx
@@ -2,23 +2,25 @@ import { Subtitle, Textfield, Button } from '../index.js';
 import './profile-form.css';
 
 export default function ProfileForm() {
+  const avatarURL =
+    'https://avatars.design/wp-content/uploads/2021/02/corporate-avatars-TN-1.jpg';
   return (
     <form className="profile-form">
       <Subtitle text="Update Profile" />
-      <fieldset>
+      <fieldset className="profile-form__fieldset">
         <Textfield name="firstName" label="First Name" />
         <Textfield name="lastName" label="Last Name" />
       </fieldset>
-      <fieldset>
+      <fieldset className="profile-form__fieldset">
         <Textfield name="city" label="City" />
         <Textfield name="country" label="Country" />
       </fieldset>
-      <fieldset>
+      <fieldset className="profile-form__fieldset">
         <Textfield name="phone" label="Phone Number" />
         <Textfield name="birthday" label="BirthDay" />
       </fieldset>
-      <fieldset>
-        <img src="" alt="avatar" />
+      <fieldset className="profile-form__fieldset">
+        <img src={avatarURL} alt="avatar" className="profile-form__avatar" />
         <Textfield name="avatar" label="Avatar URL" />
       </fieldset>
       <div className="confirm-modal__buttons">
