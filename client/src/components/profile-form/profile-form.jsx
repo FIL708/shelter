cbb285fdfa4f5c02720239ea70273303141431fd
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-07 01:08:33 +0200

    add data nad handlers

--- client/src/components/profile-form/profile-form.jsx
+++ client/src/components/profile-form/profile-form.jsx
@@ -1,27 +1,64 @@
 import { Subtitle, Textfield, Button } from '../index.js';
 import './profile-form.css';
 
-export default function ProfileForm() {
+export default function ProfileForm({ inputsValues, dataHandler }) {
   const avatarURL =
+    inputsValues.avatar ||
     'https://avatars.design/wp-content/uploads/2021/02/corporate-avatars-TN-1.jpg';
   return (
     <form className="profile-form">
       <Subtitle text="Update Profile" />
       <fieldset className="profile-form__fieldset">
-        <Textfield name="firstName" label="First Name" />
-        <Textfield name="lastName" label="Last Name" />
+        <Textfield
+          name="firstName"
+          label="First Name"
+          value={inputsValues.firstName}
+          onChange={dataHandler}
+        />
+        <Textfield
+          name="lastName"
+          label="Last Name"
+          value={inputsValues.lastName}
+          onChange={dataHandler}
+        />
       </fieldset>
       <fieldset className="profile-form__fieldset">
-        <Textfield name="city" label="City" />
-        <Textfield name="country" label="Country" />
+        <Textfield
+          name="city"
+          label="City"
+          value={inputsValues.city}
+          onChange={dataHandler}
+        />
+        <Textfield
+          name="country"
+          label="Country"
+          value={inputsValues.country}
+          onChange={dataHandler}
+        />
       </fieldset>
       <fieldset className="profile-form__fieldset">
-        <Textfield name="phone" label="Phone Number" />
-        <Textfield type="date" name="birthday" label="Date of birth" />
+        <Textfield
+          name="phone"
+          label="Phone Number"
+          value={inputsValues.phone}
+          onChange={dataHandler}
+        />
+        <Textfield
+          type="date"
+          name="birthday"
+          label="Date of birth"
+          value={inputsValues.birthday}
+          onChange={dataHandler}
+        />
       </fieldset>
       <fieldset className="profile-form__fieldset">
         <img src={avatarURL} alt="avatar" className="profile-form__avatar" />
-        <Textfield name="avatar" label="Avatar URL" />
+        <Textfield
+          name="avatar"
+          label="Avatar URL"
+          value={inputsValues.avatar}
+          onChange={dataHandler}
+        />
       </fieldset>
       <div className="confirm-modal__buttons">
         <Button text="Confirm" />
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -21,7 +21,9 @@ export default function Test() {
     city: 'Mława',
     country: 'Poland',
     phone: '123123123',
-    birthday: '04.02.1995',
+    birthday: '1995-02-05',
+    avatar:
+      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80',
   });
 
   const [passwordForm, setPasswordForm] = useState({
@@ -83,6 +85,7 @@ export default function Test() {
     const { name, value } = event.target;
     setUpdateForm((prev) => ({ ...prev, [name]: value }));
   };
+  console.log(updateForm);
 
   return (
     <Page>
