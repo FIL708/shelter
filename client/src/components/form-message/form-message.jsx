96f5b574ca846c21827ef074b45ed2834757f2d0
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-12 19:22:22 +0200

    add structure and styling

--- client/src/components/form-message/form-message.jsx
+++ client/src/components/form-message/form-message.jsx
@@ -1,5 +1,8 @@
 import './form-message.css';
 
-export default function FormMessage() {
-  return <div>form-message</div>;
+export default function FormMessage({ text, isValid }) {
+  const messageClassName = '';
+  console.log(isValid);
+
+  return <p className={messageClassName}>{text}</p>;
 }
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,14 +1,10 @@
-import { Page, Subtitle } from '../components';
-import { areObjectsEqual } from '../helpers';
+import { Page, Subtitle, FormMessage } from '../components';
 
 export default function Test() {
-  const obj1 = { a: 1, b: 2 };
-  const obj2 = { a: 1, b: 2 };
-  console.log(areObjectsEqual(obj1, obj2));
-
   return (
     <Page>
       <Subtitle text="Test Page" main />
+      <FormMessage text="its ok " />
     </Page>
   );
 }
