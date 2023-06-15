d5b71e5880760c38aaa6cfea2f7fc23e76443b3e
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-15 20:50:14 +0200

    specify icons style

--- client/src/features/adoption/opinions-list/opinions-list__item/opinions-list__item.jsx
+++ client/src/features/adoption/opinions-list/opinions-list__item/opinions-list__item.jsx
@@ -14,8 +14,8 @@ export default function OpinionsListItem({ body, user, updatedAt }) {
       </h3>
       <p className="opinions-list__item-body">{body}</p>
       <div className="opinions-list__buttons">
-        <Button iconType="edit" iconFill="#7286d3" iconSize="15px" />
-        <Button />
+        <Button iconType="edit" iconFill="#7286d3" iconSize="18px" />
+        <Button iconType="exit" iconFill="#9f3e3e" iconSize="18px" />
       </div>
     </li>
   );
