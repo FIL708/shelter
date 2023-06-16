dfaef6aa60a783b37385b431ac654e641427ec41
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-16 22:28:30 +0200

    refactor scroll button

--- client/src/components/ui/scroll-button/scroll-button.jsx
+++ client/src/components/ui/scroll-button/scroll-button.jsx
@@ -1,12 +1,14 @@
+import { useScrollToggle } from '../../../hooks';
 import { Button } from '../../form';
 
 import './scroll-button.css';
 
-export default function ScrollButton({ visible }) {
+export default function ScrollButton({ offset }) {
+  const isScrollButtonVisible = useScrollToggle(offset || 200);
   const scrollToTop = () => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   };
-  const scrollButtonClassName = visible
+  const scrollButtonClassName = isScrollButtonVisible
     ? 'scroll-button'
     : 'scroll-button inactive';
   return (
