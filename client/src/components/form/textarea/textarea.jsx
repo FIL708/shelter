e04731773bda546903d4bada487110440450b7eb
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-17 20:47:54 +0200

    textarea styling

--- client/src/components/form/textarea/textarea.jsx
+++ client/src/components/form/textarea/textarea.jsx
@@ -1,5 +1,13 @@
 import './textarea.css';
 
-export default function Textarea({ name, value }) {
-  return <textarea className="textarea" name={name} id={name} value={value} />;
+export default function Textarea({ name, value, onChange }) {
+  return (
+    <textarea
+      className="textarea"
+      name={name}
+      id={name}
+      value={value}
+      onChange={onChange}
+    />
+  );
 }
