c02315c061b126feb8991728f51584d96abb3627
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-20 09:52:22 +0200

    make opinion private route

--- server/src/routes/opinion.js
+++ server/src/routes/opinion.js
@@ -5,15 +5,26 @@ const updateOpinion = async (req, res) => {
   const { id } = req.params;
   const { body } = req.body;
   try {
+    if (!req.session.passport) {
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+    if (req.session.passport.user.role !== 'admin') {
+      if (req.session.passport.user.id.toString() !== id.toString()) {
+        return res.status(403).json({ message: 'Unauthorized' });
+      }
+    }
+
     const opinion = await Opinion.findByPk(id);
+
     if (!opinion) {
       return res.status(404).json({ message: 'Opinion not found' });
     }
     if (!body) {
       return res.status(422).json({ message: 'Unprocessable entity' });
     }
+
     await opinion.update({ body });
-    return res.status(200).json(opinion);
+    return res.status(200).json({ message: 'Opinion successfully updated' });
   } catch (error) {
     return res.status(500).json({ message: 'Something goes wrong', error });
   }
