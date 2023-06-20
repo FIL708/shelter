38ed404e022f71e7fcdfdacd3176d0c761ec27a5
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-20 10:01:11 +0200

    finished delete route

--- server/src/routes/opinion.js
+++ server/src/routes/opinion.js
@@ -37,12 +37,26 @@ const deleteOpinion = async (req, res) => {
   const { id } = req.params;
 
   try {
+    if (!req.session.passport) {
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+
     const opinion = await Opinion.findByPk(id);
+
+    if (req.session.passport.user.role !== 'admin') {
+      if (
+        req.session.passport.user.id.toString() !== opinion.userId.toString()
+      ) {
+        return res.status(403).json({ message: 'Unauthorized' });
+      }
+    }
 
     if (!opinion) {
       return res.status(404).json({ message: 'Opinion not found' });
     }
-    console.log(opinion);
+
+    await opinion.destroy();
+    return res.status(200).json({ message: 'Opinion successfully deleted' });
   } catch (error) {
     return res.status(500).json({ message: 'Something goes wrong', error });
   }
