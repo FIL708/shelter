0d1ea322669ee0b6f6e2218c645afa4744b957d7
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-25 20:42:50 +0200

    add handlers to hook

--- client/src/features/adoption/hooks/useOpinions.js
+++ client/src/features/adoption/hooks/useOpinions.js
@@ -2,11 +2,28 @@ import { useEffect, useState } from 'react';
 
 export default function useOpinions(initial) {
   const [opinions, setOpinions] = useState([]);
-  console.log(opinions);
 
   useEffect(() => {
     if (initial) {
       setOpinions(initial.opinions);
     }
   }, [initial]);
+
+  const addOpinion = (opinion) => {
+    setOpinions((prev) => [opinion, ...prev]);
+  };
+
+  const changeOpinion = (id, body) => {
+    setOpinions((prev) =>
+      prev.map((opinion) =>
+        opinion.id === id ? { ...opinion, body } : opinion,
+      ),
+    );
+  };
+
+  const deleteOpinion = (id) => {
+    setOpinions((prev) => prev.filter((opinion) => opinion.id !== id));
+  };
+
+  return [opinions, addOpinion, changeOpinion, deleteOpinion];
 }
