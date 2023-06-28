7d644b4967f4c18706e94975473f6a8bee01895c
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-28 16:25:46 +0200

    clean up test page

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,24 +1,9 @@
-import { useState } from 'react';
 import { Page, Subtitle } from '../components/ui';
-import { Textarea } from '../components/form';
-import OpinionsListItem from '../features/adoption/components/opinions-list/opinion/opinion.jsx';
 
 export default function Test() {
-  const [state, setState] = useState();
-  const onChange = (event) => {
-    const { value } = event.target;
-    setState(value);
-  };
-
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <OpinionsListItem
-        body="asdasdg asdhasdhasd hasdhasdh hasdhasd"
-        user={{ firstName: 'Filip', lastName: 'Zebrowski' }}
-        updatedAt="1994-02-01"
-      />
-      <Textarea onChange={onChange} value={state} />
     </Page>
   );
 }
