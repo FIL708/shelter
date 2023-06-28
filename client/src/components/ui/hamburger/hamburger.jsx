39d72bd58061fc66f505c95b603f2570d593b048
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-28 16:34:45 +0200

    add links

--- client/src/components/ui/hamburger/hamburger.jsx
+++ client/src/components/ui/hamburger/hamburger.jsx
@@ -1,3 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
 export default function Hamburger() {
-  return <nav className="hamburger">sdf</nav>;
+  return (
+    <nav className="hamburger">
+      <NavLink className="hamburger__link" to="/test">
+        Development
+      </NavLink>
+      <NavLink className="hamburger__link" to="/">
+        Home
+      </NavLink>
+      <NavLink className="hamburger__link" to="/adoptions">
+        Adoptions
+      </NavLink>
+      <NavLink className="hamburger__link" to="/gallery">
+        Gallery
+      </NavLink>
+      <NavLink className="hamburger__link" to="/contact">
+        Contact
+      </NavLink>
+
+      <NavLink className="hamburger__link" to="/signup">
+        Signup
+      </NavLink>
+      <NavLink className="hamburger__link" to="/login">
+        Login
+      </NavLink>
+    </nav>
+  );
 }
