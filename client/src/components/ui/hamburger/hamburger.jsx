ac16e259cbdb7247c85ff449f171dfce5b2e4203
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-28 18:07:38 +0200

    add structure to user panel

--- client/src/components/ui/hamburger/hamburger.jsx
+++ client/src/components/ui/hamburger/hamburger.jsx
@@ -1,5 +1,6 @@
 import HamburgerLink from './hamburger__link/hamburger__link.jsx';
 import HamburgerMain from './hamburger__main-links/hamburger__main-links.jsx';
+import HamburgerUserPanel from './hamburger__user-panel/hamburger__user-panel.jsx';
 
 export default function Hamburger({ user, logoutHandler }) {
   console.log(user, logoutHandler);
@@ -15,6 +16,7 @@ export default function Hamburger({ user, logoutHandler }) {
           Login
         </HamburgerLink>
       </ul>
+      <HamburgerUserPanel user={user} />
     </nav>
   );
 }
--- client/src/components/ui/hamburger/hamburger__user-panel/hamburger__user-panel.jsx
+++ client/src/components/ui/hamburger/hamburger__user-panel/hamburger__user-panel.jsx
@@ -1,18 +1,39 @@
+import HamburgerLink from '../hamburger__link/hamburger__link.jsx';
+
 import './hamburger__user-panel.css';
 
-export default function HamburgerUserPanel() {
-  //   const panelItems =
-  //     userRole === 'admin'
-  //       ? [
-  //           { linkName: 'Users', link: '/users' },
-  //           { linkName: 'Pets', link: '/pets' },
-  //           { linkName: 'Messages', link: '/messages' },
-  //           { linkName: 'My Profile', link: `/profile/${userId}` },
-  //         ]
-  //       : [
-  //           { linkName: 'Favorites pets', link: '/favorites' },
-  //           { linkName: 'Messages', link: '/messages' },
-  //           { linkName: 'My Profile', link: `/profile/${userId}` },
-  //         ];
-  return <nav className="hamburger__user-panel">asd</nav>;
+export default function HamburgerUserPanel({ user }) {
+  const panelItems =
+    user.role === 'admin'
+      ? [
+          { linkName: 'Users', link: '/users' },
+          { linkName: 'Pets', link: '/pets' },
+          { linkName: 'Messages', link: '/messages' },
+          { linkName: 'My Profile', link: `/profile/${user.id}` },
+        ]
+      : [
+          { linkName: 'Favorites pets', link: '/favorites' },
+          { linkName: 'Messages', link: '/messages' },
+          { linkName: 'My Profile', link: `/profile/${user.id}` },
+        ];
+
+  return (
+    <ul className="hamburger__user-panel">
+      <li className="hamburger__user-panel__user">
+        <img
+          src={user.avatar}
+          alt="user avatar"
+          className="hamburger__user-panel__avatar"
+        />
+        <h4 className="hamburger__user-panel__email">{user.email}</h4>
+      </li>
+      {panelItems.map((item) => (
+        <HamburgerLink
+          text={item.linkName}
+          to={item.link}
+          key={item.linkName}
+        />
+      ))}
+    </ul>
+  );
 }
