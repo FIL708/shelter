41a8a73d4c83b9bbef9dc1f98f9386e36722124c
FIL708 <filip.zebrowski4013@gmail.com>
2023-06-28 18:11:14 +0200

    add divider

--- client/src/components/ui/hamburger/hamburger.jsx
+++ client/src/components/ui/hamburger/hamburger.jsx
@@ -2,12 +2,15 @@ import HamburgerLink from './hamburger__link/hamburger__link.jsx';
 import HamburgerMain from './hamburger__main-links/hamburger__main-links.jsx';
 import HamburgerUserPanel from './hamburger__user-panel/hamburger__user-panel.jsx';
 
+import './hamburger.css';
+
 export default function Hamburger({ user, logoutHandler }) {
   console.log(user, logoutHandler);
 
   return (
     <nav className="hamburger">
       <HamburgerMain />
+      <hr className="hamburger__divider" />
       <ul>
         <HamburgerLink text="Signup" to="/signup">
           Signup
