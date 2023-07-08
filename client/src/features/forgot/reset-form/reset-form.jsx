b7f88e98e84b3633d082cdec41693f3e672bddb6
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-08 22:51:12 +0200

    create component structure

--- client/src/features/forgot/reset-form/reset-form.jsx
+++ client/src/features/forgot/reset-form/reset-form.jsx
@@ -1,9 +1,15 @@
 import { Subtitle } from 'components/ui';
+import { Button, Textfield } from 'components/form';
 
 export default function ResetForm() {
   return (
     <form className="reset-form">
       <Subtitle text="Reset password" main />
+      <fieldset>
+        <Textfield />
+        <Textfield />
+      </fieldset>
+      <Button />
     </form>
   );
 }
