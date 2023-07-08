b413f0620abc91ef5222e3c3fe2228df195f179d
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-08 22:29:46 +0200

    use forgot form on test page

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,9 +1,11 @@
 import { Page, Subtitle } from 'components/ui';
+import { ForgotForm } from 'features/forgot';
 
 export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
+      <ForgotForm />
     </Page>
   );
 }
