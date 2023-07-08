2ee1b486b07938358f0482519fa8bd4b20fd0e67
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-08 22:42:13 +0200

    styling

--- client/src/features/forgot/forgot-form/forgot-form.jsx
+++ client/src/features/forgot/forgot-form/forgot-form.jsx
@@ -7,11 +7,14 @@ export default function ForgotForm() {
   return (
     <form className="forgot-form">
       <Subtitle text="Forgot your password?" main />
-      <p className="forgot-form__description">
-        Please enter the email address you`d like to password reset information
-        sent to
-      </p>
-      <Textfield placeholder="Enter your email address" />
+      <fieldset>
+        <p className="forgot-form__description">
+          Please enter the email address you`d like to password reset
+          information sent to
+        </p>
+        <Textfield placeholder="Enter your email address" />
+      </fieldset>
+
       <Button text="Reset password" />
     </form>
   );
