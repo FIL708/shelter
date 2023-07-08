c15c20b5d7936e388f303837eaed5548f3e61ab2
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-08 22:55:38 +0200

    add style to reset form

--- client/src/features/forgot/reset-form/reset-form.jsx
+++ client/src/features/forgot/reset-form/reset-form.jsx
@@ -1,18 +1,20 @@
 import { Subtitle } from 'components/ui';
 import { Button, Textfield } from 'components/form';
 
+import './reset-form.css';
+
 export default function ResetForm() {
   return (
     <form className="reset-form">
       <Subtitle text="Reset password" main />
-      <fieldset>
+      <fieldset className="reset-form__fieldset">
         <Textfield label="Password" placeholder="Enter your new password" />
         <Textfield
           label="Confirm password"
           placeholder="Confirm your password"
         />
       </fieldset>
-      <Button />
+      <Button text="Reset password" />
     </form>
   );
 }
