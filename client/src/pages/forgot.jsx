8d23646f78bff6a914f1d7e65a6222977a1c8a00
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-08 23:10:35 +0200

    add handlers

--- client/src/pages/forgot.jsx
+++ client/src/pages/forgot.jsx
@@ -5,14 +5,23 @@ import { useState } from 'react';
 
 export default function Forgot() {
   const { id } = useParams();
-  const [forgotValue, setForgotValue] = useState();
-  const [resetValues, setResetValues] = useState();
+  const [forgotValue, setForgotValue] = useState('');
+  const [resetValues, setResetValues] = useState({ password: '', confirm: '' });
   console.log(forgotValue, setForgotValue, resetValues, setResetValues, id);
+
+  const forgotValueHandler = (event) => {
+    const { value } = event.target;
+    setForgotValue(value);
+  };
+  const resetValuesHandler = (event) => {
+    const { value, name } = event.target;
+    setForgotValue((prev) => ({ ...prev, [name]: value }));
+  };
 
   return (
     <Page>
-      <ForgotForm />
-      <ResetForm />
+      <ForgotForm handler={forgotValueHandler} />
+      <ResetForm handler={resetValuesHandler} />
     </Page>
   );
 }
