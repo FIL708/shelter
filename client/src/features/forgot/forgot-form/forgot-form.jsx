56e2c62dd68384a2502bec26c9e854dff464f1e6
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-08 22:39:03 +0200

    styling hole container

--- client/src/features/forgot/forgot-form/forgot-form.jsx
+++ client/src/features/forgot/forgot-form/forgot-form.jsx
@@ -9,7 +9,7 @@ export default function ForgotForm() {
       <Subtitle text="Forgot your password?" main />
       <p className="forgot-form__description">
         Please enter the email address you`d like to password reset information
-        sent to.
+        sent to
       </p>
       <Textfield placeholder="Enter your email address" />
       <Button text="Reset password" />
