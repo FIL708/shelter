707e71cce735d5f37ca33bbd4a14c13ddee4594d
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-09 14:01:36 +0200

    add send mail reoute

--- server/src/routes/forgot.js
+++ server/src/routes/forgot.js
@@ -5,4 +5,14 @@ const checkForgotSession = (req, res) => {
   return res.status(200).json({ id });
 };
 
-module.exports = Router().get('/:id', checkForgotSession);
+const sendForgotEmail = (req, res) => {
+  const { email } = req.body;
+
+  if (!email) return res.status(422).json({ message: 'Invalid data provided' });
+
+  return res.status(200).json({ message: 'Email successfully sended' });
+};
+
+module.exports = Router()
+  .get('/:id', checkForgotSession)
+  .post('/', sendForgotEmail);
