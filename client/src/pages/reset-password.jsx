ed5a8e9f779e76f3e4e0f1ef054d4e7fec70c825
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-12 17:54:59 +0200

    add logic to reset password page

--- client/src/pages/reset-password.jsx
+++ client/src/pages/reset-password.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { Page } from 'components/ui';
 import { ResetForm } from 'features/forgot';
 
 export default function ResetPassword() {
+  const [resetValues, setResetValues] = useState({ password: '', confirm: '' });
+  const [formMessage, setFormMessage] = useState({ text: '', isValid: false });
+  console.log(setFormMessage);
+
+  const resetValuesHandler = (event) => {
+    const { value, name } = event.target;
+    setResetValues((prev) => ({ ...prev, [name]: value }));
+  };
+  const sendResetRequest = () => {
+    console.log('Reset!');
+  };
   return (
     <Page>
-      <ResetForm />
+      <ResetForm
+        inputValues={resetValues}
+        handler={resetValuesHandler}
+        resetPassword={sendResetRequest}
+        message={formMessage}
+      />
     </Page>
   );
 }
