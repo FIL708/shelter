5fa3384b8f65626dcb2b7b7316816f80e6929c68
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-12 18:02:12 +0200

    add error handling

--- client/src/pages/reset-password.jsx
+++ client/src/pages/reset-password.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
-import { Page } from 'components/ui';
+import { ErrorCard, LoadingSpinner, Page, Subtitle } from 'components/ui';
+import { useFetch } from 'hooks';
 import { ResetForm } from 'features/forgot';
+import { useParams } from 'react-router-dom';
 
 export default function ResetPassword() {
+  const { id } = useParams();
+  const [pets, isLoading, error] = useFetch(`/api/forgot/${id}`);
   const [resetValues, setResetValues] = useState({ password: '', confirm: '' });
   const [formMessage, setFormMessage] = useState({ text: '', isValid: false });
-  console.log(setFormMessage);
+  console.log(setFormMessage, pets, isLoading, error);
 
   const resetValuesHandler = (event) => {
     const { value, name } = event.target;
@@ -14,6 +18,18 @@ export default function ResetPassword() {
   const sendResetRequest = () => {
     console.log('Reset!');
   };
+
+  if (isLoading || error)
+    return (
+      <Page>
+        <Subtitle text="Our Pets" main />
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <ErrorCard errorCode={error.status} errorMessage={error.message} />
+        )}
+      </Page>
+    );
   return (
     <Page>
       <ResetForm
