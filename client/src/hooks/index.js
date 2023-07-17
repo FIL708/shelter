a06143fdb8741733ed49c5f0227e51734b2ebdd5
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-17 23:08:52 +0200

    create files

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useScrollToggle } from './use-scroll-toggle.js';
 export { default as useFetch } from './use-fetch.js';
 export { default as useWindowSize } from './use-window-size.js';
+export { default as useValidation } from './use-validation.js';
