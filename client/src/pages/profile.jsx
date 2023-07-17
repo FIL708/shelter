bf10d8611fe6c1a8f417f510dfa26ea82d6e7498
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-18 00:07:14 +0200

    useValidation in user page

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -9,46 +9,16 @@ import {
 } from 'components/ui';
 import { ChangePasswordForm, ProfileForm, UserCard } from 'features/profile';
 import { UserContext } from 'index.jsx';
-import { useFetch } from 'hooks';
-import { inputValidator, areObjectsEqual } from 'utils';
+import { useFetch, useValidation } from 'hooks';
+import { areObjectsEqual } from 'utils';
 
 export default function Profile() {
   const { id } = useParams();
   const navigate = useNavigate();
   const { serverUrl } = useContext(UserContext);
-  const [user, isLoading, error] = useFetch(`/api/user/${id}`);
-  const initialIsFormValid = {
-    passwordForm: {
-      currentPassword: {
-        isValid: null,
-        message: null,
-      },
-      newPassword: {
-        isValid: null,
-        message: null,
-      },
-      confirmPassword: {
-        isValid: null,
-        message: null,
-      },
-    },
-    updateForm: {
-      phone: {
-        isValid: null,
-        message: null,
-      },
-      avatar: {
-        isValid: null,
-        message: null,
-      },
-    },
-  };
 
-  const [visibleModals, setVisibleModals] = useState({
-    confirm: false,
-    password: false,
-    update: false,
-  });
+  const [user, isLoading, error] = useFetch(`/api/user/${id}`);
+
   const [updateForm, setUpdateForm] = useState({
     previous: {
       firstName: '',
@@ -73,15 +43,50 @@ export default function Profile() {
       avatar: '',
     },
   });
+  const [isUpdateFormValid, updateValidationHandler, updateValidationReset] =
+    useValidation({
+      phone: {
+        isValid: null,
+        message: '',
+      },
+      avatar: {
+        isValid: null,
+        message: '',
+      },
+    });
   const [passwordForm, setPasswordForm] = useState({
     currentPassword: '',
     newPassword: '',
     confirmPassword: '',
   });
-  const [isFormsValid, setIsFormValid] = useState(initialIsFormValid);
+
+  const [
+    isPasswordFormValid,
+    passwordValidationHandler,
+    passwordValidationReset,
+  ] = useValidation({
+    currentPassword: {
+      isValid: null,
+      message: null,
+    },
+    newPassword: {
+      isValid: null,
+      message: null,
+    },
+    confirmPassword: {
+      isValid: null,
+      message: null,
+    },
+  });
+
   const [formsMessage, setFormsMessage] = useState({
     text: '',
     isWrong: false,
+  });
+  const [visibleModals, setVisibleModals] = useState({
+    confirm: false,
+    password: false,
+    update: false,
   });
 
   useEffect(() => {
@@ -133,112 +138,48 @@ export default function Profile() {
     const { name, value } = event.target;
     setPasswordForm((prev) => ({ ...prev, [name]: value }));
   };
-  const passwordValidationHandler = (event, type, confirm) => {
-    const { name, value } = event.target;
-    const validationObject = inputValidator(value, type, confirm);
-    setIsFormValid((prev) => ({
-      ...prev,
-      passwordForm: { ...prev.passwordForm, [name]: validationObject },
-    }));
-  };
   const onClosingPasswordForm = () => {
     togglePasswordModal();
-    setIsFormValid((prev) => ({
-      ...prev,
-      passwordForm: initialIsFormValid.passwordForm,
-    }));
+    passwordValidationReset();
     setPasswordForm({
       currentPassword: '',
       newPassword: '',
       confirmPassword: '',
     });
   };
-  const checkRequiredPasswordFormFields = () => {
-    let isFilled = true;
-    if (!passwordForm.currentPassword) {
-      setIsFormValid((prev) => ({
-        ...prev,
-        passwordForm: {
-          ...prev.passwordForm,
-          currentPassword: {
-            isValid: false,
-            message: 'This field is required',
-          },
-        },
-      }));
-      isFilled = false;
-    }
-    if (!passwordForm.newPassword) {
-      setIsFormValid((prev) => ({
-        ...prev,
-        passwordForm: {
-          ...prev.passwordForm,
-          newPassword: {
-            isValid: false,
-            message: 'This field is required',
-          },
-        },
-      }));
-      isFilled = false;
-    }
-    if (!passwordForm.confirmPassword) {
-      setIsFormValid((prev) => ({
-        ...prev,
-        passwordForm: {
-          ...prev.passwordForm,
-          confirmPassword: {
-            isValid: false,
-            message: 'This field is required',
-          },
-        },
-      }));
-      isFilled = false;
-    }
-    return isFilled;
-  };
   const onConfirmPasswordForm = async () => {
-    const isFormFilled = checkRequiredPasswordFormFields();
-    if (isFormFilled) {
-      const res = await fetch(`/api/user/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password: passwordForm }),
-      });
+    const res = await fetch(`/api/user/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ password: passwordForm }),
+    });
 
-      if (res.status === 401) {
-        const respondMessage = await res.json();
-        setIsFormValid((prev) => ({
-          ...prev,
-          passwordForm: {
-            ...prev.passwordForm,
-            currentPassword: {
-              message: respondMessage.message,
-              isValid: false,
-            },
-          },
-        }));
-      } else if (!res.ok) {
-        setFormsMessage({ text: 'Something goes wrong', isWrong: true });
-      } else {
+    if (res.status === 401) {
+      setFormsMessage({
+        text: 'The entered current password is incorrect',
+        isWrong: true,
+      });
+    } else if (!res.ok) {
+      setFormsMessage({ text: 'Something goes wrong', isWrong: true });
+    } else {
+      setFormsMessage({
+        text: 'Password successfully changed',
+        isWrong: false,
+      });
+      setPasswordForm({
+        currentPassword: '',
+        newPassword: '',
+        confirmPassword: '',
+      });
+      passwordValidationReset();
+
+      setTimeout(() => {
+        togglePasswordModal();
         setFormsMessage({
-          text: 'Password successfully changed',
+          text: '',
           isWrong: false,
         });
-        setPasswordForm({
-          currentPassword: '',
-          newPassword: '',
-          confirmPassword: '',
-        });
-        setIsFormValid(initialIsFormValid);
-
-        setTimeout(() => {
-          togglePasswordModal();
-          setFormsMessage({
-            text: '',
-            isWrong: false,
-          });
-        }, 1000);
-      }
+      }, 1000);
     }
   };
 
@@ -263,21 +204,9 @@ export default function Profile() {
       }));
     }
   };
-  const updateValidationHandler = (event, type) => {
-    const { name, value } = event.target;
-    const validationObject = inputValidator(value, type);
-    setIsFormValid((prev) => ({
-      ...prev,
-      updateForm: { ...prev.updateForm, [name]: validationObject },
-    }));
-  };
   const onClosingUpdateForm = () => {
     toggleUpdateModal();
-    setIsFormValid((prev) => ({
-      ...prev,
-      updateForm: initialIsFormValid.updateForm,
-    }));
-    setPasswordForm({ password: '', confirm: '' });
+    updateValidationReset();
     setUpdateForm((prev) => ({ ...prev, current: prev.previous }));
     setFormsMessage({ text: '', isWrong: false });
   };
@@ -297,10 +226,7 @@ export default function Profile() {
             isWrong: false,
           });
           setUpdateForm((prev) => ({ ...prev, previous: prev.current }));
-          setIsFormValid((prev) => ({
-            ...prev,
-            updateForm: initialIsFormValid.updateForm,
-          }));
+          updateValidationReset();
         }
       }
 
@@ -350,7 +276,7 @@ export default function Profile() {
       />
       <ChangePasswordForm
         validationHandler={passwordValidationHandler}
-        validationObject={isFormsValid.passwordForm}
+        validationObject={isPasswordFormValid}
         dataHandler={passwordFormDataHandler}
         inputsValues={passwordForm}
         isVisible={visibleModals.password}
@@ -365,7 +291,7 @@ export default function Profile() {
         toggleModalVision={toggleUpdateModal}
         isVisible={visibleModals.update}
         validationHandler={updateValidationHandler}
-        validationObject={isFormsValid.updateForm}
+        validationObject={isUpdateFormValid}
         onCancel={onClosingUpdateForm}
         onConfirm={onConfirmUpdateForm}
         message={formsMessage}
