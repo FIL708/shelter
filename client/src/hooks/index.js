be3f9ad4631a4da7cfae9aecf736ef4c8dbd8859
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-19 23:33:51 +0200

    create useForm file

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useScrollToggle } from './use-scroll-toggle.js';
 export { default as useFetch } from './use-fetch.js';
 export { default as useWindowSize } from './use-window-size.js';
 export { default as useValidation } from './use-validation.js';
+export { default as useForm } from './use-form.js';
