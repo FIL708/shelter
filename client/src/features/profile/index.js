146fe5d7cc5544d2e6f9d7c2657351cab2ad49d6
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-21 12:06:01 +0200

    move profile features files to components folder

--- client/src/features/profile/index.js
+++ client/src/features/profile/index.js
@@ -1,3 +1,3 @@
-export { default as ChangePasswordForm } from './change-password-form/change-password-form.jsx';
-export { default as ProfileForm } from './profile-form/profile-form.jsx';
-export { default as UserCard } from './user-card/user-card.jsx';
+export { default as ChangePasswordForm } from './components/change-password-form/change-password-form.jsx';
+export { default as ProfileForm } from './components/profile-form/profile-form.jsx';
+export { default as UserCard } from './components/user-card/user-card.jsx';
