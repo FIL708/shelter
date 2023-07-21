7d6818c6aad184b97dc8e21c2ffb92495e391665
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-21 12:25:26 +0200

    set state, handler and reset

--- client/src/features/profile/hooks/useProfileForm.js
+++ client/src/features/profile/hooks/useProfileForm.js
@@ -1,3 +1,55 @@
+import { useState } from 'react';
+
+const profileInit = {
+  previous: {
+    firstName: '',
+    lastName: '',
+    address: {
+      city: '',
+      country: '',
+    },
+    phone: '',
+    birthday: '',
+    avatar: '',
+  },
+  current: {
+    firstName: '',
+    lastName: '',
+    address: {
+      city: '',
+      country: '',
+    },
+    phone: '',
+    birthday: '',
+    avatar: '',
+  },
+};
+
 export default function useProfileForm() {
-  return [];
+  const [profileForm, setProfileForm] = useState(profileInit);
+
+  const profileFormHandler = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'city' || name === 'country') {
+      setProfileForm((prev) => ({
+        ...prev,
+        current: {
+          ...prev.current,
+          address: { ...prev.current.address, [name]: value },
+        },
+      }));
+    } else {
+      setProfileForm((prev) => ({
+        ...prev,
+        current: { ...prev.current, [name]: value },
+      }));
+    }
+  };
+
+  const setPrevProfileValues = () => {
+    setProfileForm((prev) => ({ ...prev, current: prev.previous }));
+  };
+
+  return [profileForm, profileFormHandler, setPrevProfileValues];
 }
