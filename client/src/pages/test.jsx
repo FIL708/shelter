4094c19cba3be4298298f88d92e4c7dc0cff929a
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-23 01:00:25 +0200

    create import

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,9 +1,10 @@
-import { Page, Subtitle } from 'components/ui';
+import { Page, Subtitle, AdoptionForm } from 'components/ui';
 
 export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
+      <AdoptionForm />
     </Page>
   );
 }
