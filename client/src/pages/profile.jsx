c0ce92001e3ee9adb7da483c8be14c63001d0117
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-23 00:29:17 +0200

    clean up code

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -60,7 +60,6 @@ export default function Profile() {
   ] = useProfileForm(user);
   const [isUpdateFormValid, updateValidationHandler, updateValidationReset] =
     useValidation(updateValidationInit);
-  console.log(profileForm);
 
   const [passwordForm, passwordFormHandler, passwordFormReset] =
     useForm(passwordFormInit);
