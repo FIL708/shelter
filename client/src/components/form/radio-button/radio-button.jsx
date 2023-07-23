97737db68801c4990dc0301a7879e801807caadb
FIL708 <filip.zebrowski4013@gmail.com>
2023-07-23 23:23:49 +0200

    working on radio button

--- client/src/components/form/radio-button/radio-button.jsx
+++ client/src/components/form/radio-button/radio-button.jsx
@@ -1,9 +1,16 @@
 import './radio-button.css';
 
-export default function RadioButton({ name, checked, value, onChange }) {
-  const labelClassName = checked
-    ? 'checked radio-button__label'
-    : 'radio-button__label';
+export default function RadioButton({
+  name,
+  checked,
+  value,
+  onChange,
+  variant,
+}) {
+  const labelClassName =
+    checked && variant === 'button'
+      ? 'checked radio-button__button-type'
+      : 'radio-button__button-type';
   return (
     <label htmlFor={name} className={labelClassName}>
       {value}
