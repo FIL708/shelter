89cc8403ce196536fac954debb48dac569cc3cf5
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-02 00:02:10 +0200

    create adoption form hook

---
+++ client/src/hooks/use-adoption-form.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+const init = {
+  name: '',
+  description: '',
+  shortDescription: '',
+  gender: '',
+  age: '',
+  weight: '',
+  species: '',
+  createdAt: '',
+  address: {
+    city: '',
+    country: '',
+  },
+  photos: [],
+};
+
+export default function useAdoptionForm(adoption) {
+  const [adoptionForm, setAdoptionForm] = useState(adoption || init);
+
+  useEffect(() => {
+    if (adoption) {
+      setAdoptionForm(adoption);
+    }
+  }, [adoption]);
+
+  const formHandler = (event) => {
+    const { name, value } = event.target;
+    if (name === 'city' || name === 'country') {
+      setAdoptionForm((prev) => ({
+        ...prev,
+        address: { ...prev.address, [name]: value },
+      }));
+    } else if (name === 'photos') {
+      setAdoptionForm((prev) => ({
+        ...prev,
+        photos: value.split(' ').map((url) => ({ url })),
+      }));
+    } else {
+      setAdoptionForm((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+  return [adoptionForm, formHandler];
+}
