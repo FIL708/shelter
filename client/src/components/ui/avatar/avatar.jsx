c0d2e87c278f30a64b41f59c4cf9cfadd4eab6d8
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-20 00:43:06 +0200

    pass photo prop

--- client/src/components/ui/avatar/avatar.jsx
+++ client/src/components/ui/avatar/avatar.jsx
@@ -1,5 +1,8 @@
 import './avatar.css';
 
-export default function Avatar() {
-  return <div>A</div>;
+export default function Avatar({ photo }) {
+  const url =
+    photo ||
+    'https://tleliteracy.com/wp-content/uploads/2017/02/default-avatar.png';
+  return <img src={url} alt="user avatar" />;
 }
