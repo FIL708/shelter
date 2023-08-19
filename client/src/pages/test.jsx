18f8a48cbbcecf7ef3fcd7c59c98d9d6f45e408c
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-20 01:09:24 +0200

    add styling

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -4,7 +4,10 @@ export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <Avatar />
+      <Avatar
+        photo="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80"
+        size="small"
+      />
     </Page>
   );
 }
