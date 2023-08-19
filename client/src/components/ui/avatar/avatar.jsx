657f4598f4c57539fddd9582e59d3ca754ce9e7d
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-20 00:54:19 +0200

    add default avatar image

--- client/src/components/ui/avatar/avatar.jsx
+++ client/src/components/ui/avatar/avatar.jsx
@@ -1,8 +1,8 @@
 import './avatar.css';
 
-export default function Avatar({ photo }) {
-  const url =
-    photo ||
-    'https://tleliteracy.com/wp-content/uploads/2017/02/default-avatar.png';
-  return <img src={url} alt="user avatar" />;
+export default function Avatar({
+  photo = 'https://tleliteracy.com/wp-content/uploads/2017/02/default-avatar.png',
+  alt = 'user avatar',
+}) {
+  return <img src={photo} alt={alt} />;
 }
