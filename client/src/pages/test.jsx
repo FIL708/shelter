d88a8b5048ab9db407c6bc69fb1d30af996b8525
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-26 01:01:12 +0200

    add styling

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -4,7 +4,10 @@ export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <Alert variant="success" message="Message" />
+      <Alert
+        variant="success"
+        message="Message asd asd asd asdasdasd asdasdasd asdasdasd asdasd"
+      />
       <Alert variant="warning" message="Message" />
       <Alert variant="info" message="Message" />
       <Alert variant="error" message="Message" />
