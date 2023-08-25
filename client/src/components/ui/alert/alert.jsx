521d4872477b1c0b773dfa606b3b2cd60189d00c
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-26 00:31:35 +0200

    add structure

--- client/src/components/ui/alert/alert.jsx
+++ client/src/components/ui/alert/alert.jsx
@@ -1,7 +1,10 @@
 import './alert.css';
 
 export default function Alert({ variant, message }) {
-  console.log(variant, message);
-
-  return <aside className="alert">alert</aside>;
+  return (
+    <aside className="alert">
+      <h4 className="alert__header">{variant}</h4>
+      <strong className="alert__message">{message}</strong>
+    </aside>
+  );
 }
