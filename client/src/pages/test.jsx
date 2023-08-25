bf6772bbc4d640c80b525ca580ca5d5c4ba401d0
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-26 00:48:06 +0200

    fix icons

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -5,6 +5,9 @@ export default function Test() {
     <Page>
       <Subtitle text="Test Page" main />
       <Alert variant="success" message="Message" />
+      <Alert variant="warning" message="Message" />
+      <Alert variant="info" message="Message" />
+      <Alert variant="error" message="Message" />
     </Page>
   );
 }
