75dfe912af949237035647e07b3151225c2e6bcd
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-26 00:25:38 +0200

    pass props

--- client/src/components/ui/alert/alert.jsx
+++ client/src/components/ui/alert/alert.jsx
@@ -1,5 +1,7 @@
 import './alert.css';
 
-export default function Alert() {
+export default function Alert({ variant, message }) {
+  console.log(variant, message);
+
   return <aside className="alert">alert</aside>;
 }
--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,13 +1,10 @@
-import { Page, Subtitle, Avatar } from 'components/ui';
+import { Page, Subtitle, Alert } from 'components/ui';
 
 export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <Avatar
-        photo="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80"
-        size="small"
-      />
+      <Alert variant="success" message="Message" />
     </Page>
   );
 }
