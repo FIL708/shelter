e870d8162a7150ef3afe05752fd9f53d602ede72
FIL708 <filip.zebrowski4013@gmail.com>
2023-08-31 01:23:55 +0200

    add alert animation

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -4,13 +4,7 @@ export default function Test() {
   return (
     <Page>
       <Subtitle text="Test Page" main />
-      <Alert
-        variant="success"
-        message="Message asd asd asd asdasdasd asdasdasd asdasdasd asdasd"
-      />
-      {/* <Alert variant="warning" message="Message" />
-      <Alert variant="info" message="Message" />
-      <Alert variant="error" message="Message" /> */}
+      <Alert variant="success" message="" />
     </Page>
   );
 }
