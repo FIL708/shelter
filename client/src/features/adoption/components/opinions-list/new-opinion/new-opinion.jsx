b1d92aa6261d023f4e18ca8b5ad0020d09791aa5
FIL708 <filip.zebrowski4013@gmail.com>
2023-09-14 01:00:25 +0200

    add new comp tonew opininon

--- client/src/features/adoption/components/opinions-list/new-opinion/new-opinion.jsx
+++ client/src/features/adoption/components/opinions-list/new-opinion/new-opinion.jsx
@@ -1,4 +1,4 @@
-import { Button, Textarea } from 'components/form';
+import { IconButton, Textarea } from 'components/form';
 import './new-opinion.css';
 
 export default function NewOpinion({
@@ -16,10 +16,10 @@ export default function NewOpinion({
         onChange={(event) => newOpinionHandler(event)}
         value={newOpinion}
       />
-      <Button
-        iconType="send"
-        iconFill="#7286d3"
-        iconSize="25px"
+      <IconButton
+        type="send"
+        colorl="#7286d3"
+        size="25px"
         disabled={!newOpinion}
         onClick={() => createOpinion(newOpinion)}
       />
