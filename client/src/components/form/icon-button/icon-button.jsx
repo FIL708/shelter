b5317e22a4efdcaf7478dc24a59eb12b8d254435
FIL708 <filip.zebrowski4013@gmail.com>
2023-09-14 00:37:20 +0200

    add styling

--- client/src/components/form/icon-button/icon-button.jsx
+++ client/src/components/form/icon-button/icon-button.jsx
@@ -1,10 +1,19 @@
 import { Icon } from 'components/ui';
 import './icon-button.css';
 
-export default function IconButton({ onClick }) {
+export default function IconButton({
+  onClick,
+  type = 'heart',
+  size = '20px',
+  // color = '#7286d3',
+  background = 'primary',
+}) {
+  const buttonClassName = `icon-button ${background}`;
+  let iconColor;
+
   return (
-    <button type="button" className="button-icon" onClick={onClick}>
-      <Icon />
+    <button type="button" className={buttonClassName} onClick={onClick}>
+      <Icon type={type} size={size} fill={iconColor} />
     </button>
   );
 }
