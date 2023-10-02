a8566861d21cc8e752902ec1350dd15328d43da6
FIL708 <filip.zebrowski4013@gmail.com>
2023-10-02 12:07:48 +0200

    clean up code

--- client/src/features/profile/components/user-card/user-card__data/user-card__data.jsx
+++ client/src/features/profile/components/user-card/user-card__data/user-card__data.jsx
@@ -8,56 +8,31 @@ export default function UserCardData({ userData }) {
   const formattedCreatedAt = getFormattedDate(userData.createdAt);
 
   return (
-    <div className="user-card__data">
-      <div className="user-card__data__lists">
-        <ul className="user-card__data__list">
-          <li className="user-card__data__label">Email:</li>
-          <li className="user-card__data__label">Phone:</li>
-          <li className="user-card__data__label">City:</li>
-          <li className="user-card__data__label">Country:</li>
-          <li className="user-card__data__label">Birthday:</li>
-          <li className="user-card__data__label">Created at:</li>
-        </ul>
-        <ul className="user-card__data__list">
-          <li className="user-card__data__data">{userData.email}</li>
-          <li className="user-card__data__data">{userData.phone || '-'}</li>
-          <li className="user-card__data__data">
-            {userData.address?.city || '-'}
-          </li>
-          <li className="user-card__data__data">
-            {userData.address?.country || '-'}
-          </li>
-          <li className="user-card__data__data">{formattedBirthday}</li>
-          <li className="user-card__data__data">{formattedCreatedAt}</li>
-        </ul>
-      </div>
-
-      <ul className="user-card__data-list">
-        <li className="user-card__data__item-label">
-          <span>Email:</span>
-          {userData.email}
-        </li>
-        <li className="user-card__data__item-label">
-          <span>Phone:</span>
-          {userData.phone || '-'}
-        </li>
-        <li className="user-card__data__item-label">
-          <span>City:</span>
-          {userData.address?.city || '-'}
-        </li>
-        <li className="user-card__data__item-label">
-          <span>Country:</span>
-          {userData.address?.country || '-'}
-        </li>
-        <li className="user-card__data__item-label">
-          <span>Birthday:</span>
-          {formattedBirthday}
-        </li>
-        <li className="user-card__data__item-label">
-          <span>Created at:</span>
-          {formattedCreatedAt}
-        </li>
-      </ul>
-    </div>
+    <ul className="user-card__data-list">
+      <li className="user-card__data__item">
+        <span className="user-card__data__item-label">Email:</span>
+        {userData.email}
+      </li>
+      <li className="user-card__data__item">
+        <span className="user-card__data__item-label">Phone:</span>
+        {userData.phone || '-'}
+      </li>
+      <li className="user-card__data__item">
+        <span className="user-card__data__item-label">City:</span>
+        {userData.address?.city || '-'}
+      </li>
+      <li className="user-card__data__item">
+        <span className="user-card__data__item-label">Country:</span>
+        {userData.address?.country || '-'}
+      </li>
+      <li className="user-card__data__item">
+        <span className="user-card__data__item-label">Birthday:</span>
+        {formattedBirthday}
+      </li>
+      <li className="user-card__data__item">
+        <span className="user-card__data__item-label">Created at:</span>
+        {formattedCreatedAt}
+      </li>
+    </ul>
   );
 }
